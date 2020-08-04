bf3ac0deb80b3df82e68cfddb68e3c9c40909445
subniw00 <tnqls5417@naver.com>
2020-08-04 23:45:00 +0900

    20200804 extra

---
+++ baekjoon/그 외 문제/[9656].cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int N, dp[1001] = { 0 };
+int find_dp(int n) {
+	if (dp[n] != 0) return dp[n];
+	if (find_dp(n - 3)== -1 || find_dp(n - 1) == -1)
+		return dp[n] = 1;
+	else  return dp[n] = -1;
+}
+int main() {//µ¹ °ÔÀÓ2
+	cin >> N;
+	dp[1] = -1;
+	dp[2] = 1;
+	dp[3] = -1;
+	if (find_dp(N) == 1)
+		cout << "SK";
+	else if (find_dp(N) == -1)
+		cout << "CY";
+	return 0;
+}
