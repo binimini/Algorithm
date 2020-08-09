5b12f213f2f76d13f78dcbf45e0ea4127771d4b1
Subin Min <tnqls5417@naver.com>
2020-08-10 02:00:44 +0900

    20200809 CR_663[A,B]

---
+++ codeforces/CR_663[0809]/CR663A.cpp
@@ -0,0 +1,14 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int T,N;
+int main() {
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        cin >> N;
+        for (int i = 1; i<=N; i++){
+            cout << i << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
---
+++ codeforces/CR_663[0809]/CR663B.cpp
@@ -0,0 +1,33 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int T, N, M, arr[101][101] = { 0 };
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        cin >> N >> M;
+        char temp;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cin >> temp;
+                if (temp == 'R')
+                    arr[i][j] = 1;
+                else if (temp == 'D')
+                    arr[i][j] = -1;
+                else arr[i][j] = 0;
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i][M - 1] != -1)
+                cnt++;
+        }
+        for (int j = 0; j < M; j++) {
+            if (arr[N - 1][j] != 1)
+                cnt++;
+        }
+        cout << cnt - 2 << "\n";
+    }
+    return 0;
+}
