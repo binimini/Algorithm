5a5682b0d5573415fa4d7f5014b4c1ea06e3a392
Subin Min <tnqls5417@naver.com>
2020-08-10 21:57:19 +0900

    20200810

---
+++ baekjoon/동적계획법[0810]/[1757].cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int N, M, d[10001], dp[10002][501] = { 0 }, Max = -10000001;//배열 크기 조건 확실히..
+int find_max(int n, int status) {
+    if (n > N+1)
+        return Max;
+    if (status > M)
+        return Max;
+    if (n == N+1) {
+        if (status ==0)
+            return 0;
+        else return Max;
+    }
+    if (dp[n][status] != -1)
+        return dp[n][status];
+    if (status > 0) 
+       return dp[n][status] = max(find_max(n + 1, status + 1) + d[n], find_max(n + status, 0));
+    else if (status==0) 
+       return dp[n][status] = max(find_max(n + 1,status+1)+d[n], find_max(n+1, 0));
+}
+int main() {//달려달려
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        cin >> d[i];
+    }
+    memset(dp, -1, sizeof(dp));
+    cout << find_max(1,0) << "\n";
+    return 0;
+}
---
+++ baekjoon/동적계획법[0810]/[1760].cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int N, dp[31] = { 0 };
+int case_(int n) {
+    if (n < 0)
+        return 0;
+    if (n == 0)
+        return 1;
+    if (dp[n] != 0) 
+        return dp[n];
+    return dp[n] = case_(n - 2)*2 + case_(n - 1);
+}
+int main() {//타일 코드
+    cin >> N;
+    int symm;
+    if (N % 2 != 0)
+        symm = case_((N - 1) / 2);
+    else symm = case_(N / 2) + 2*case_(N/2-1);
+    cout << (case_(N) - symm) / 2 + symm << '\n';
+    //결과 = (전체 케이스 - symm한 케이스) / 2 + symm한 케이스
+    return 0;
+}
