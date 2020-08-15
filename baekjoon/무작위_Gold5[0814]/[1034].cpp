ddb8b1ef373340ac6ed656d7f10d7135ccac3cad
Subin Min <tnqls5417@naver.com>
2020-08-15 21:33:17 +0900

    20200814

---
+++ baekjoon/무작위_Gold5[0814]/[1034].cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+#define FOR(x,a,b) for (int x = a; x<b; x++)
+
+int N, M,K, arr[50][50];
+pair<int, int> info[50];
+
+int main() {//램프
+    cin >> N >> M;
+
+    char temp;
+    FOR(i, 0, N) {
+        info[i].first = 0, info[i].second = 1;
+        FOR(j, 0, M) {
+            cin >> temp;
+            arr[i][j] = temp - '0';
+            if (arr[i][j] == 0)
+                info[i].first++;
+        }
+    }
+    cin >> K;
+    
+    FOR(i, 0, N) {
+        if (info[i].first == -1) continue;
+        FOR(j, i+1, N) {
+            if (info[j].first == -1) continue;
+            int CH = 1;
+            FOR(k, 0, M) {
+                if (arr[i][k] != arr[j][k]) {
+                    CH = 0;
+                    break;
+                }
+            }
+    
+            if (CH) {
+                info[i].second++;
+                info[j].first = -1;
+            }
+        }
+    }
+    int result = 0;
+    for (int i = 0; i < N; i++) {
+        if (info[i].first == -1) continue;
+     
+        if ((K - info[i].first) % 2 == 0 && info[i].first <= K)
+            result = max(result, info[i].second);
+    }
+    cout << result;
+    return 0;
+}
---
+++ baekjoon/무작위_Gold5[0814]/[1043].cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+#define FOR(i,a,b) for(int i = a; i<b; i++)
+int N, M, T;
+vector<int> t_people;
+vector<int> p_info[50];
+vector<int>pp_info[51];
+bool party[50] = { 0 }, people[51] = { 0 };
+
+int main() {//거짓말
+    cin >> N >> M;
+    cin >> T;
+    int temp;
+    FOR(i, 0, T) {
+        cin >> temp;
+        t_people.push_back(temp);
+    }
+    FOR(i, 0, M) {
+        cin >> temp;
+        int t;
+        FOR(j, 0, temp) {
+            cin >> t;
+            p_info[i].push_back(t);
+            pp_info[t].push_back(i);
+        }
+    }
+    queue<int> q;
+    FOR(i, 0, t_people.size()) {
+        q.push(t_people[i]);
+    }
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        people[x] = 1;
+        FOR(i, 0, pp_info[x].size()) {
+            if (party[pp_info[x][i]] == 1) continue;
+            party[pp_info[x][i]] = 1;
+            FOR(j, 0, p_info[pp_info[x][i]].size()) {
+                if (people[p_info[pp_info[x][i]][j]] == 1) continue;
+                q.push(p_info[pp_info[x][i]][j]);
+            }
+        }
+    }
+    int cnt = 0;
+    FOR(i, 0, M) {
+        if (party[i] == 0) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
