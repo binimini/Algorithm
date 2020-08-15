4a2a6e28362bef306989855b96133276ee7a59b7
Subin Min <tnqls5417@naver.com>
2020-08-15 21:32:14 +0900

    20200814 ECR_93[A,B]

---
+++ codeforces/ECR_93[0814]/ECR93A.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int T, N;
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        cin >> N;
+        int temp;
+        int a, b, c;
+        for (int i = 0; i < N; i++) {
+            cin >> temp;
+            if (i == 0)
+                a = temp;
+            else if (i == 1)
+                b = temp;
+            else if (i == N - 1)
+                c = temp;
+        }
+        if (a + b <= c)
+            cout << 1 << " " << 2 << " " << N << "\n";
+        else cout << -1 << "\n";
+    }
+    return 0;
+}
---
+++ codeforces/ECR_93[0814]/ECR93B.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int T;
+vector<int> one;
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T;
+    string s;
+    for (int t = 0; t < T; t++) {
+        cin >> s;
+        int cnt = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1')
+                cnt++;
+            else {
+                one.push_back(cnt);
+                cnt = 0;
+            }
+        }
+        if (cnt != 0)
+            one.push_back(cnt);
+        sort(one.begin(), one.end());
+        int result = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (one.empty())
+                break;
+            if (i % 2 == 0)
+                result += one.back();
+            one.pop_back();
+        }
+        cout << result << '\n';
+    }
+    return 0;
+}
