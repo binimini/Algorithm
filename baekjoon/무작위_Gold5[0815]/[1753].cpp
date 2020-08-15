2e54226f3d2adee57641cefa868beccb43aa26e0
Subin Min <tnqls5417@naver.com>
2020-08-15 21:33:51 +0900

    20200815

---
+++ baekjoon/무작위_Gold5[0815]/[1753].cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int V, E, K, INF = 987654321;
+vector<pair<int, int>> graph[20001];
+vector<int> dijkstra(int s, int v) {
+    vector<int> dist(v, INF);
+    dist[s] = 0;
+    priority_queue<pair<int, int>> pq;//cost, vertex // pq 설정 바꿀시 부호 안바꿔도
+    pq.push(make_pair(0, s));
+    while (!pq.empty()) {
+        int cost = -pq.top().first;//현재 비용
+        int curV = pq.top().second;//현재 노드
+        pq.pop();
+
+        if (dist[curV] < cost) continue;//최소거리보다 현재 비용이 더 크면 패스
+
+        for (int i = 0; i < graph[curV].size(); i++) {
+            int neighbor = graph[curV][i].first;//인접 노드
+            int neighborDist = cost + graph[curV][i].second;// 인접 노드 거리(현재 노드 통한)
+
+            if (dist[neighbor] > neighborDist) {//더 작으면 업데이트
+                dist[neighbor] = neighborDist;
+                pq.push(make_pair(-neighborDist, neighbor));//우선순위큐에 집어넣음 
+            }
+        }
+    }
+    return dist;
+}
+int main() {//최단경로
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> V >> E >> K;
+    int a, b, c;
+    for (int i = 0; i < E; i++) {
+        cin >> a >> b >> c;
+        graph[a].push_back(make_pair(b, c));//노드, 비용
+    }
+    
+    vector<int> result = dijkstra(K,V+1);
+    
+    for (int i = 1; i <= V; i++) {
+        if (result[i] == INF)
+            cout << "INF\n";
+        else cout << result[i] << '\n';
+    }
+    return 0;
+}
---
+++ baekjoon/무작위_Gold5[0815]/[1759].cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<string> result;
+int L, C, arr[16];
+bool visited[16] = { 0 };
+char ch[16];
+void DFS(int node, int cnt) {
+    if (cnt == L) {
+        string s = "";
+        int a_cnt = 0, b_cnt = 0;
+        for (int i = 0; i < cnt; i++) {
+            if (ch[arr[i]] == 'a' || ch[arr[i]] == 'e' || ch[arr[i]] == 'i' || ch[arr[i]] == 'o' || ch[arr[i]] == 'u')
+                a_cnt++;
+            else b_cnt++;
+            s += "0";
+        }
+        
+        if (!(a_cnt >= 1 && b_cnt >= 2)) return;
+        result.push_back(s);
+        for (int i = 0; i < cnt; i++) {
+            result[result.size()-1][i] =  ch[arr[i]];
+        }
+        return;
+    }
+    for (int i = node; i < C; i++) {
+        arr[cnt] = i;
+        DFS(i+1, cnt + 1);
+    }
+}
+int main() {//암호 만들기
+    cin >> L >> C;
+    for (int i = 0; i < C; i++) {
+        cin >> ch[i];
+    }
+    sort(ch, ch + C);
+    DFS(0, 0);
+    sort(result.begin(), result.end());
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << "\n";
+    }
+    return 0;
+}
