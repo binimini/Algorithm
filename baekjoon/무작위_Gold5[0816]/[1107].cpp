326d45a54e53436c84bb1c0c953b0036c6686838
Subin Min <tnqls5417@naver.com>
2020-08-16 20:57:03 +0900

    20200816

---
+++ baekjoon/무작위_Gold5[0816]/[1107].cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+int N, M, INF = 987654321;
+bool broken[10] = { 0 };
+int main() {//리모컨
+    cin >> N >> M;
+    int temp;
+    for (int i = 0; i < M; i++) {
+        cin >> temp;
+        broken[temp] = 1;
+    }
+    int case1 = abs(N - 100);
+    int case2 = INF;
+    for (int i = 0; i < 1000000; i++) {
+        int temp_i = i, cnt = 0;
+        bool CH = 0;
+        while (temp_i != 0) {
+            cnt++;
+            if (broken[temp_i % 10] == 1) { 
+                CH = 1;
+                break;
+            }
+            temp_i /= 10;
+        }
+        if (i == 0) {
+            if (broken[0] == 1)
+                CH = 1;
+            cnt = 1;
+        }
+        if (CH == 0) {
+            case2 = min(abs(N - i) + cnt, case2);
+        }
+    }
+    cout << min(case1, case2);
+    return 0;
+}
---
+++ baekjoon/무작위_Gold5[0816]/[3055].cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <queue>
+#include <tuple>
+using namespace std;
+int R, C, CH = 0;
+int map[50][50] = { 0 }, visited[50][50] = { 0 }, d[4][2] = { { -1,0 },{1,0},{0,-1},{0,1} };
+tuple<int, int, int> start;
+queue<tuple<int, int, int>> q;
+void BFS() {
+    while (!q.empty()) {
+        int x = get<0>(q.front());
+        int y = get<1>(q.front());
+        int z = get<2>(q.front());
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int nx = x + d[i][0];
+            int ny = y + d[i][1];
+            if (z == 1) {//고슴도치
+                if (nx<0 || nx>R - 1 || ny<0 || ny>C - 1) continue;
+                if (map[nx][ny] == -1) continue;
+                if (visited[nx][ny]) continue;
+                visited[nx][ny] = visited[x][y] + 1;
+                if (map[nx][ny] == 1) {
+                    CH = 1;
+                    cout << visited[nx][ny] - 1;
+                    return;
+                }
+                q.push(make_tuple(nx, ny, 1));
+            }
+            else if (z == -1) {//물
+                if (nx<0 || nx>R - 1 || ny<0 || ny>C - 1) continue;
+                if (map[nx][ny]) continue;
+                map[nx][ny] = -1;
+                q.push(make_tuple(nx, ny, -1));
+                
+
+            }
+        }
+    }
+    if (CH == 0)
+        cout << "KAKTUS";
+}
+int main() {//탈출
+    cin >> R >> C;
+    char temp;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> temp;
+            if (temp == 'S')
+                start = make_tuple(i, j, 1);
+            else if (temp == '*')
+                q.push(make_tuple(i, j,-1));
+            else if (temp == 'D')
+                map[i][j] = 1;
+            else if (temp == 'X')
+                map[i][j] = -1;
+        }
+    }
+    q.push(start);
+    visited[get<0>(start)][get<1>(start)] = 1;
+    BFS();
+    return 0;
+}
