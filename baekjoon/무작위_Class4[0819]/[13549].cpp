d8e5c19c4c0c7835579def82646cbbc2a60f210e
Subin Min <tnqls5417@naver.com>
2020-08-19 23:55:37 +0900

    20200819

---
+++ baekjoon/무작위_Class4[0819]/[13549].cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+using namespace std;
+priority_queue<pair<int,int>> pq;;
+int N, K;
+int d[3] = { 0,-1,1 }, INF = 9876543, dist[100001];
+void dijkstra() {//각 간선의 가중치 다르므로(*2, +1, +1) BFS 불가
+    while (!pq.empty()) {
+        int xcost = -pq.top().first;
+        int x = pq.top().second;
+
+        pq.pop();
+        if (xcost > dist[x]) continue;
+        for (int i = 0; i < 3; i++) {
+            int nx = x + d[i];
+            int nxcost = xcost+1;
+            if (!i)
+                nx = x * 2, nxcost--;
+            if (nx < 0 || nx>100000) continue;
+
+            if (nxcost < dist[nx]) {
+                dist[nx] = nxcost;
+                pq.push(make_pair(-nxcost, nx));
+            }
+        }
+    }
+}
+int main() {//숨바꼭질 3
+    cin >> N >> K;
+    memset(dist, INF, sizeof(dist));
+    dist[N] = 0;
+    pq.push(make_pair(0,N));
+    dijkstra();
+    cout << dist[K];
+    return 0;
+}
---
+++ baekjoon/무작위_Class4[0819]/[2263].cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int N, postOrder[100000], inOrder[100000], idx[100000];
+void preOrder(int inBegin, int inEnd, int postBegin, int postEnd) {
+    if (inEnd < inBegin || postEnd < postBegin) return;
+    int root = postOrder[postEnd];//postOrder의 끝 == 루트
+    cout << root << " ";
+    int rootidx = idx[root];//루트의 inOrder에서 index
+    preOrder(inBegin, rootidx - 1, postBegin, postBegin + (rootidx - inBegin) - 1);//inBegin~root까지의 개수 맞춰서 postidx 정하기 
+    preOrder(rootidx + 1, inEnd, postBegin + (rootidx - inBegin), postEnd - 1);//나머지 개수 //postEnd 사용했으므로 제외
+}
+int main() {//트리의 순회
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        cin >> inOrder[i];
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> postOrder[i];
+    }
+    for (int i = 0; i < N; i++) {
+        idx[inOrder[i]] = i;
+    }
+    preOrder(0, N - 1, 0, N - 1);
+    return 0;
+}
