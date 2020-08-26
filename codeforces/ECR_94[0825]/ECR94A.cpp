d3af06d87a20e910d5e4c70be5b524bb9b99fe61
Subin Min <tnqls5417@naver.com>
2020-08-26 19:24:02 +0900

    20200825 ECR_94[A,C]

---
+++ codeforces/ECR_94[0825]/ECR94A.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+int T, N;
+int main() {
+    FASTIO;
+    cin >> T;
+    FOR(t, 0, T) {
+        cin >> N;
+        char temp;
+        int key;
+        for (int i = 0; i < 2 * N - 1; i++) {
+            cin >> temp;
+            if (i == N - 1)
+                key = temp-'0';
+        }
+        for (int i = 0; i < N; i++) {
+            cout << key;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
---
+++ codeforces/ECR_94[0825]/ECR94C.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+int T, X;
+string S;
+int main() {
+    FASTIO;
+    cin >> T;
+    FOR(t, 0, T) {
+        cin >> S >> X;
+        string W = "";
+        string NS = "";
+        for (int i = 0; i < S.size(); i++) {
+            W += '1';
+            NS += '0';
+        }
+        for (int i = 0; i < S.size(); i++) {
+            if (S[i] == '0') {
+                if (i + X < S.size()) {
+                    W[i + X] = '0';
+                }
+                if (i - X >= 0) {
+                    W[i - X] = '0';
+                }
+            }
+        }
+        for (int i = 0; i < S.size(); i++) {
+            if (i+X<S.size()) {
+                if (W[i+X]=='1')
+                    NS[i] = '1';
+            }
+            if (i - X >= 0) {
+                if (W[i-X]=='1')
+                    NS[i] = '1';
+            }
+        }
+        if (NS == S) {
+            cout << W << '\n';
+        }
+        else cout << -1 << '\n';
+    }
+    return 0;
+}
