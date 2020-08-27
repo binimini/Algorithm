31eb4cda6baf804cbd61a3dad134c7e952c76b23
Subin Min <tnqls5417@naver.com>
2020-08-27 22:40:35 +0900

    20200827

---
+++ baekjoon/무작위_Class4[0827]/[14938].cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+priority_queue < pair<int, int>> pq;
+int N, M, R, INF = 987654321;
+int item[101], dist[101];
+vector<pair<int, int>> edge[101];
+void dijkstra() {
+    while (!pq.empty()) {
+        int V = pq.top().second;
+        int VC = -pq.top().first;
+        pq.pop();
+        if (dist[V] < VC) continue;
+        for (int i = 0; i < edge[V].size(); i++) {
+            int N = edge[V][i].first;
+            int NC = edge[V][i].second;
+            if (dist[N] > VC + NC) {
+                dist[N] = VC + NC;
+                pq.push(make_pair(-dist[N], N));
+            }
+        }
+    }
+}
+int main() {//서강그라운드
+    cin >> N >> M >> R;
+    for (int i = 1; i <= N; i++) {
+        cin >> item[i];
+    }
+    int a, b, c;
+    for (int i = 0; i < R; i++) {
+        cin >> a >> b >> c;
+        edge[a].push_back(make_pair(b, c));
+        edge[b].push_back(make_pair(a, c));
+    }
+    int result = 0;
+    for (int i = 1; i <= N; i++) {
+        int cnt = 0;
+        for (int j = 1; j <= N; j++) {
+            dist[j] = INF;
+        }
+        dist[i] = 0;
+        pq.push(make_pair(0, i));
+        dijkstra();
+        for (int j = 1; j <= N; j++) {
+            if (dist[j] <= M)
+                cnt += item[j];
+        }
+        result = max(cnt, result);
+    }
+    cout << result << '\n';
+    return 0;
+}
---
+++ baekjoon/무작위_Class4[0827]/[18119].cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int N, M;
+int memory = (1<<27) -1;
+vector<int> v;
+int main() {//단어 암기
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N >> M;
+    string s;
+    for (int i = 0; i < N; i++) {
+        cin >> s;
+        int  temp = 0;
+        for (int j = 0; j < s.size(); j++) {
+            temp |= 1 << (s[j] - 'a');
+        }
+        v.push_back(temp);
+    }
+    int a;
+    char b;
+    for (int i = 0; i < M; i++) {
+        cin >> a >> b;
+        if (a == 1)
+            memory &= ~(1 << (b - 'a'));
+        else if (a == 2)
+            memory |= 1 << (b - 'a');
+        int cnt = 0;
+        for (int j = 0; j < v.size(); j++) {
+            if ((memory & v[j]) == v[j])
+                cnt++;
+        }
+        cout << cnt << '\n';
+    }
+    return 0;
+}
