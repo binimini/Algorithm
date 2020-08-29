90c3718c3cf9c74bb4064a1975b45cb55bca9081
Subin Min <tnqls5417@naver.com>
2020-08-29 22:02:09 +0900

    20200828

---
+++ baekjoon/무작위_Class5[0828]/[10942].cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+int N, M;
+int dp[2000][2000];
+int arr[2000];
+int query(int s, int e) {
+    if (s > e)
+        return 1;
+    if (dp[s][e] != -1)
+        return dp[s][e];
+    if (s == e)
+        return dp[s][e] = 1;
+    if (arr[s] != arr[e])
+        return dp[s][e] = 0;
+    return dp[s][e] = query(s + 1, e - 1);
+}
+int main() {//팰린드롬?
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    memset(dp, -1, sizeof(dp));
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    cin >> M;
+    int S, E;
+    for (int i = 0; i < M; i++) {
+        cin >> S >> E;
+        cout << query(S-1, E-1) << "\n";
+    }
+    return 0;
+}
---
+++ baekjoon/무작위_Class5[0828]/[1197].cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int V, E, cnt = 0, R = 0;
+bool visited[10001] = { 0 };
+vector<pair<int, int>> edge[10001];
+priority_queue<pair<int, int>> pq;
+void prim() {
+    while (!pq.empty()) {
+        int x = pq.top().second;
+        int xc = -pq.top().first;
+        pq.pop();
+        if (visited[x]) continue;
+        visited[x] = 1;
+        R += xc;
+        cnt++;
+        if (cnt == V) return;
+        for (int i = 0; i < edge[x].size(); i++) {
+            if (visited[edge[x][i].first]) continue;
+            pq.push(make_pair(-edge[x][i].second, edge[x][i].first));
+        }
+    }
+}
+int main() {//최소 스패닝 트리
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> V >> E;
+    int a, b, c;
+    for (int i = 0; i < E; i++) {
+        cin >> a >> b >> c;
+        edge[a].push_back(make_pair(b, c));
+        edge[b].push_back(make_pair(a, c));
+    }
+
+    pq.push(make_pair(0, 1));
+    prim();
+    cout << R << '\n';
+    return 0;
+}
