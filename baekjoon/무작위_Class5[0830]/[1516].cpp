cd93b3c847a48eb5cdaeeb63c322e275b0b13b0b
Subin Min <tnqls5417@naver.com>
2020-08-30 23:34:41 +0900

    20200830

---
+++ baekjoon/무작위_Class5[0830]/[1516].cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int N, time[501], deg[501], result[501], ex[501] = { 0 };
+vector<int> v[501];
+queue<int> q;
+void topology() {
+    for (int i = 1; i <= N; i++) {
+        if (deg[i] == 0)
+            q.push(i);
+    }
+    for (int i = 1; i <= N; i++) {
+        if (q.empty())
+            break;
+        int x = q.front();
+        q.pop();
+        result[x] = time[x] + ex[x];
+        for (int j = 0; j < v[x].size(); j++) {
+            deg[v[x][j]]--;
+            ex[v[x][j]] = max(ex[v[x][j]], result[x]);
+            if (deg[v[x][j]] == 0) {
+                q.push(v[x][j]);
+            }
+        }
+    }
+    
+}
+int main() {//게임 개발
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        int temp;
+        cin >> temp;
+        time[i] = temp;
+        while (1) {
+            cin >> temp;
+            if (temp == -1)
+                break;
+            v[temp].push_back(i);
+            deg[i]++;
+        }
+    }
+    topology();
+    for (int i = 1; i <= N; i++) {
+        cout << result[i] << '\n';
+    }
+    return 0;
+}
---
+++ baekjoon/무작위_Class5[0830]/[7453].cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int N;
+int A[4000], B[4000], C[4000], D[4000];
+vector<int> X, Y;
+int lower_bound(int key) {
+    int start = 0;
+    int end = Y.size();
+    int mid = Y.size();
+    while (end - start > 0) {
+        mid = (start + end) / 2;
+        if (Y[mid] < key)
+            start = mid + 1;
+        if (Y[mid] >= key)
+            end = mid;
+    }
+    return end;
+}
+int upper_bound(int key) {
+    int start = 0;
+    int end = Y.size();
+    int mid = Y.size();
+    while (end - start > 0) {
+        mid = (start + end) / 2;
+        if (Y[mid] <= key)
+            start = mid + 1;
+        if (Y[mid] > key)
+            end = mid;
+    }
+    return end;
+
+}
+int main() {//합이 0인 네 정수
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i] >> B[i] >> C[i] >> D[i];
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            Y.push_back(C[i] + D[j]);
+        }
+    }
+    sort(Y.begin(), Y.end());
+    long long int result = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            result += upper_bound(-(A[i] + B[j]));
+            result -= lower_bound(-(A[i] + B[j]));
+        }
+    }
+    cout << result << "\n";
+    return 0;
+}
