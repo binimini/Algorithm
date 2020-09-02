376f4932b75d76f0ce2f6ce290672393cec19a63
Subin Min <tnqls5417@naver.com>
2020-09-02 23:39:52 +0900

    20200902

---
+++ baekjoon/[0902] 무작위_Class5/[1202] 보석 도둑.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int N, K, bag[300000];
+pair<int, int> jw[300000];
+priority_queue<int> pq;
+//정렬해서 lower_bound 사용?
+int main() {
+    FASTIO;
+    cin >> N >> K;
+    FOR(i, 0, N)
+        cin >> jw[i].first >> jw[i].second;
+    FOR(i, 0, K)
+        cin >> bag[i];
+    sort(bag, bag + K);
+    sort(jw, jw + N);
+    int j = 0;
+    long long int result = 0;
+    FOR(i, 0, K) {//이후 K당 보석은 모두 **공통** //정렬시 이전 검사 부분 검사 필요 없음
+                  //무게 작은한에서 최대 가치 -> pq?
+        while (j<N&&jw[j].first<=bag[i]) {
+            pq.push(jw[j].second);
+            j++;
+        }
+        if (!pq.empty()) {
+            result += pq.top();
+            pq.pop();
+        }
+    }
+    cout << result << "\n";
+    return 0;
+}
---
+++ baekjoon/[0902] 무작위_Class5/[1509] 펠린드롬 분할.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+#define FOR(i,a,b); for (int i = a; i<b; i++)
+string S;
+int dp[2500], INF = 987654321;
+bool ispail[2500][2500] = { 0 };
+int main() {
+    cin >> S;
+    FOR(i, 0, S.size()) {
+        FOR(j, 0, i+1) {//j부터 i까지 페일드롬인지 여부
+            if (i == j) {
+                ispail[i][j] = 1;
+                continue;
+            }
+            if (S[i] == S[j]) {
+                if (j + 1 == i)
+                    ispail[i][j] = 1;
+                else 
+                    ispail[i][j] = ispail[i - 1][j + 1];//j+1 - i-1까지는 j i(i>=j) 이미 i = j-1일때값 정함
+            }
+        }
+    }
+    
+    FOR(i, 0, S.size()) {
+        dp[i] = 987654321;
+        FOR(j, 0, i+1) {
+            if (ispail[i][j]) {//최소 자기자신은 페일드롬 //i 당 한 경우는 존재
+                if (j == 0)
+                    dp[i] = min(dp[i], 1);//0 - i 경우
+                else dp[i] = min(dp[j - 1] + 1, dp[i]);//dp[i] = dp[j-1] + 1(j - i) or dp[i]
+            }
+        }
+    }
+    cout << dp[S.size() - 1];
+    return 0;
+}
