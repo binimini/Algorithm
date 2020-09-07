c710e94009608c964e8ed3f4f7b60c021a5a5abf
Subin Min <tnqls5417@naver.com>
2020-09-07 19:50:57 +0900

    20200906 CR_668[A,B]

---
+++ codeforces/[0906] CR_668/CR668A.cpp
@@ -0,0 +1,21 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+int T, N;
+int arr[100];
+int main() {
+    FASTIO;
+    cin >> T;
+    FOR(t, 0,T) {
+        cin >> N;
+        FOR(i, 0, N) {
+            cin >> arr[i];
+        }
+        for (int i = N - 1; i >= 0; i--) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
---
+++ codeforces/[0906] CR_668/CR668B.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+int T, N;
+int arr[100000];
+int main() {
+    FASTIO;
+    cin >> T;
+    FOR(t, 0, T) {
+        cin >> N;
+        long long int sum = 0, cost = 0;
+        FOR(i, 0, N) {
+            cin >> arr[i];
+            if (arr[i] > 0) {
+                sum += arr[i];
+            }
+            else {
+                sum += arr[i];
+                if (sum < 0) {
+                    cost -= sum;
+                    sum = 0;
+                }
+            }
+        }
+        cout <<cost<< '\n';
+    }
+    return 0;
+}
