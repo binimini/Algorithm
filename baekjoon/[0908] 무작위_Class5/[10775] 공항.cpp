578f8ebd3b8750318f6831847a5e390bc7a445e3
Subin Min <tnqls5417@naver.com>
2020-09-08 22:54:13 +0900

    20200908

---
+++ baekjoon/[0908] 무작위_Class5/[10775] 공항.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0);
+using namespace std;
+int gate[100001];
+int G, P;
+int find(int x) {
+    if (gate[x] ==x)
+        return x;
+    return gate[x] = find(gate[x]);
+}
+void union_gate(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x < y)
+        gate[y] = x;
+    else
+        gate[x] = y;
+}
+int main() {
+    FASTIO;
+    cin >> G >> P;
+    for (int i = 1; i <= G; i++) {
+        gate[i] = i;
+    }
+    int temp, ans = P;
+    for (int i = 1; i <= P; i++) {
+        cin >> temp;
+        temp = find(temp);
+        if (!temp) {
+            ans = i - 1;
+            break;
+        }
+        else 
+            union_gate(temp - 1, temp);
+    }
+    for (int i = 0; i < P - (ans + 1); i++) {
+        cin >> temp;
+    }
+    cout << ans << '\n';
+    return 0;
+}
---
+++ baekjoon/[0908] 무작위_Class5/[2143] 두 배열의 합.cpp
@@ -0,0 +1,74 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0);
+int T, N, M, cnt = 0;
+int A[1000], B[1000];
+vector<int> SoA, SoB;
+int upper_bound(int key) {
+    int s = 0, e = SoB.size(); //들어온 값이 가장 클 때 최대인덱스+1 반환
+    int m;
+    while (e - s > 0) {//e 최대인덱스+1이므로 s==e이면 없는 인덱스에 접근하게됨
+        m = (s + e) / 2;
+        if (SoB[m] <= key) {
+            s = m + 1;
+        }
+        else
+            e = m;
+    }
+    return e;
+}
+int lower_bound(int key) {
+    int s = 0, e = SoB.size();
+    int m;
+    while (e - s > 0) {
+        m = (s + e) / 2;
+        if (SoB[m] < key) {
+            s = m + 1;
+        }
+        else
+            e = m;
+    }
+    return e;
+}
+int main() {
+    cin >> T;
+    cin >> N;
+    int sum = 0;
+    FOR(i, 0, N) {
+        cin >> A[i];
+        sum += A[i];
+        A[i] = sum;
+    }
+    cin >> M;
+    sum = 0;
+    FOR(i, 0, M) {
+        cin >> B[i];
+        sum += B[i];
+        B[i] = sum;
+    }
+    FOR(i, 0, N) {
+        FOR(j, i, N) {
+            if (i != 0)
+                SoA.push_back(A[j] - A[i - 1]);
+            else SoA.push_back(A[j]);
+        }
+    }
+    FOR(i, 0, M) {
+        FOR(j, i, M) {
+            if (i != 0)
+                SoB.push_back(B[j] - B[i - 1]);
+            else SoB.push_back(B[j]);
+        }
+    }
+    sort(SoB.begin(), SoB.end());
+    long long int cnt = 0;
+    for (int i = 0; i < SoA.size(); i++) {
+        int temp1 = upper_bound(T - SoA[i]);
+        int temp2 = lower_bound(T - SoA[i]);
+        cnt += temp1;
+        cnt -=temp2;
+    }
+    cout << cnt << '\n';
+    return 0;
+}
