8780c32686343e11718007fe2318e47a90c3c5cc
Subin Min <tnqls5417@naver.com>
2020-09-11 23:11:20 +0900

    20200911

---
+++ baekjoon/[0911] 무작위_Class5/[2342] Dance Dance Revolution.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int comm[100001];
+int INF = 1000000;
+int dp[100001][5][5];
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n = 1, temp;
+    while (1) {
+        cin >> temp;
+        if (!temp)
+            break;
+        comm[n] = temp;
+        n++;
+    }
+    for (int i = 1; i <= 100000; i++) {
+        for (int j = 0; j <= 4; j++) {
+            for (int k = 0; k <= 4; k++) {
+                dp[i][j][k] = INF;
+            }
+        }
+    }
+    dp[0][0][0] = 0;
+    dp[1][comm[1]][0] = 2;
+    dp[1][0][comm[1]] = 2;
+    for (int i = 2; i < n; i++) {
+        for (int j = 4; j >= 0; j--) {
+            for (int k = 4; k >= 0; k--) {
+                dp[i][j][k] = min(dp[i][j][k], dp[i - 1][0][k] + 2);
+                dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][0] + 2);
+                dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][k] + 1);
+                if (k == 1) {
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][4] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][2] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][3] + 4);
+                }
+                else if (k == 2) {
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][1] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][3] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][4] + 4);
+                }
+                else if(k==3){
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][2] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][4] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][1] + 4);
+                }
+                else{
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][1] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][3] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][2] + 4);
+                }
+                if (j == 1) {
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][4][k] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][2][k] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][3][k] + 4);
+                }
+                else if (j == 2) {
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][1][k] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][3][k] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][4][k] + 4);
+                }
+                else if (j == 3) {
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][2][k] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][4][k] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][1][k] + 4);
+                }
+                else {
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][1][k] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][3][k] + 3);
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][2][k] + 4);
+                }
+                if (j != comm[i] && k != comm[i])
+                    dp[i][j][k] = INF;
+            }
+        }
+    }
+    int result = INF;
+    for (int i = 0; i <= 4; i++) {
+        for (int j = 0; j <= 4; j++) {
+            result = min(dp[n - 1][i][j], result);
+        }
+    }
+    cout << result << "\n";
+    return 0;
+}
---
+++ baekjoon/[0911] 무작위_Class5/[2473] 세 용액.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <tuple>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int N;
+long long int INF = 3000000001;//int 범위 초과 
+int arr[5000];
+int lower_bound(int key) {
+    int s = 0, e = N;
+    int m;
+    while (e - s > 0) {
+        m = (s + e) / 2;
+        if (arr[m] < key) {
+            s = m + 1;
+        }
+        else e = m;
+    }
+    return e;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+
+    long long int temp1[4], temp2[4], r[4], result[4];//최대 10억+10억+10억 long long
+    result[0] = INF;
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            int idx = lower_bound(-(arr[i]+arr[j]));//두 개 합 벡터에 넣고 나열하게되면 int[25000000] 메모리 부족
+
+            temp1[0] = INF;
+            temp2[0] = INF;
+            r[0] = INF;
+
+            int temp = idx;
+            while (!(temp != i && temp != j) && temp < N) {
+                temp++;
+            }
+            if (temp != N) {
+                temp1[0] = arr[temp];
+                temp1[0] += arr[i];
+                temp1[0] += arr[j];
+                temp1[1] = temp;
+                temp1[2] = i;
+                temp1[3] = j;
+            }
+            temp = idx - 1;
+            while (!(temp != i && temp != j ) && temp >= 0) {
+                temp--;
+            }
+            if (temp >= 0) {
+                temp2[0] = arr[temp];
+                temp2[0] += arr[i];
+                temp2[0] += arr[j];
+                temp2[1] = temp;
+                temp2[2] = i;
+                temp2[3] = j;
+            }
+
+            if (abs(temp1[0]) < abs(temp2[0])) {
+                r[0] = temp1[0];
+                r[1] = temp1[1];
+                r[2] = temp1[2];
+                r[3] = temp1[3];
+            }
+            else {
+                r[0] = temp2[0];
+                r[1] = temp2[1];
+                r[2] = temp2[2];
+                r[3] = temp2[3];
+            }
+            if (abs(result[0]) > abs(r[0])) {
+                result[0] = r[0];
+                result[1] = arr[r[1]];
+                result[2] = arr[r[2]];
+                result[3] = arr[r[3]];
+            }
+        }
+    }
+    sort(result + 1, result + 4);
+    for (int i = 1; i <= 3; i++) {
+        cout << result[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
