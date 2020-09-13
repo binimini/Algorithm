aa884442a8f911823a7402b2115a4264110ee256
Subin Min <tnqls5417@naver.com>
2020-09-13 19:29:36 +0900

    20200912 CR_670[A,B]

---
+++ codeforces/[0912] CR_670/CR670A.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+int T, N;
+int cnt[101] = { 0 };
+int main() {
+    FASTIO;
+    cin >> T;
+    FOR(t, 0, T) {
+        for (int i = 0; i <= 100; i++) {
+            cnt[i] = 0;
+        }
+        cin >> N;
+        int temp;
+        FOR(i,0,N){
+            cin >> temp;
+            cnt[temp]++;
+        }
+        int a = 0;
+        while (1) {
+            if (cnt[a] != 0) {
+                cnt[a]--;
+                a++;
+            }
+            else if (cnt[a] == 0)
+                break;
+        }
+        int b = 0;
+        while (1) {
+            if (cnt[b] != 0) {
+                cnt[b]--;
+                b++;
+            }
+            else if (cnt[b] == 0)
+                break;
+        }
+        cout << a + b << "\n";
+    }
+    return 0;
+}
---
+++ codeforces/[0912] CR_670/CR670B.cpp
@@ -0,0 +1,130 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+int T, N;
+int arr[100000];
+vector<int> pqp;
+vector<int> pqm;
+int main() {
+    cin >> T;
+    FOR(t, 0, T) {
+        cin >> N;
+        pqp.clear();
+        pqm.clear();
+        int cnt0 = 0, cntp = 0, cntm = 0;
+        long long int result = 1;
+        FOR(i, 0, N) {
+            cin >> arr[i];
+            if (arr[i] > 0) {
+                cntp++;
+                pqp.push_back(arr[i]);
+            }
+            else if (arr[i] < 0) {
+                cntm++;
+                pqm.push_back(-arr[i]);
+            }
+            else if (arr[i] == 0)
+                cnt0++;
+
+        }
+        if (N == 5) {
+            FOR(i, 0, 5) {
+                result *= arr[i];
+            }
+            cout << result << "\n";
+        }
+        else {
+            int cnt = 0;
+            if (cntp==0&&cnt0+cntm>=5) {
+                if (cnt0 != 0)
+                    cout << 0 << "\n";
+                else {
+                    sort(arr, arr + N);
+                    int temp = N - 1;
+                    for (int i = 0; i < 5; i++) {
+                        result *= arr[temp];
+                        temp--;
+                    }
+                    cout << result << "\n";
+                }
+            }
+            else if ((cntm / 2) * 2 + cntp >= 5) {
+                sort(pqp.begin(), pqp.end());
+                sort(pqm.begin(), pqm.end());
+                result *= pqp.back();
+                cnt++;
+                pqp.pop_back();
+                int tempp = 0, tempm = 0;
+                while (pqp.size()>=2 && pqm.size() >= 2) {
+                    if (cnt == 5)
+                        break;
+                    if (tempp == 0) {
+                        if (pqp.size()>=2) {
+                            tempp = pqp.back()* pqp[pqp.size() - 2];
+                        }
+                    }
+                    if (tempm == 0) {
+                        if (pqm.size() >= 2) {
+                            tempm = pqm.back() * pqm[pqm.size() - 2];
+                        }
+                    }
+                    if (tempp > tempm) {
+                        result *= tempp;
+                        tempp = 0;
+                        cnt += 2;
+                        pqp.pop_back();
+                        pqp.pop_back();
+                    }
+                    else {
+                        result *= tempm;
+                        tempm = 0;
+                        cnt += 2;
+                        pqm.pop_back();
+                        pqm.pop_back();
+                    }
+                }
+                if (cnt == 5) {
+                    cout<< result << '\n';
+                }
+                else if (cnt == 3) {
+                    int t1;
+                    if (pqp.size() >= 2) {
+                        t1 = pqp.back() * pqp[pqp.size() - 2];
+                        result *= t1;
+                    }
+                    else if (pqm.size() >= 2) {
+                        t1 = pqm.back() * pqm[pqm.size() - 2];
+                        result *= t1;
+                    }
+                    cout << result << '\n';
+                }
+                else if (cnt == 1) {
+                    int t1;
+                    while (cnt != 5) {
+                        if (pqp.size() >= 2) {
+                            t1 = pqp.back() * pqp[pqp.size() - 2];
+                            result *= t1;
+                            cnt += 2;
+                            pqp.pop_back();
+                            pqp.pop_back();
+
+                        }
+                        else if (pqm.size() >= 2) {
+                            t1 = pqm.back() * pqm[pqm.size() - 2];
+                            result *= t1;
+                            cnt += 2;
+                            pqm.pop_back();
+                            pqm.pop_back();
+                        }
+                    }
+                    cout << result << '\n';
+                }
+            }
+            else if (cntm+cnt0 + cntp >= 5) {
+                cout << "0" << '\n';
+            }
+        }
+    }
+    return 0;
+}
