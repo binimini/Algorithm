1e01bd915c9f7e56db9a878cca60d3b8f4270f7c
Subin Min <tnqls5417@naver.com>
2020-09-13 23:15:36 +0900

    20200913

---
+++ baekjoon/[0913] 무작위_Class5/[1007] 벡터 매칭.cpp
@@ -0,0 +1,45 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int T, N;
+pair<int, int> xy[20];
+bool visited[20] = { 0 };
+long long int INF = 100000000000, result = INF;
+void DFS(int cnt, int node) {
+    if (cnt == N / 2) {
+        long long int xx = 0, yy = 0;
+        for (int i = 0; i < N; i++) {
+            if (visited[i] == 1)
+                xx -= xy[i].first, yy -= xy[i].second;
+            else
+                xx += xy[i].first, yy += xy[i].second;
+        }
+        long long int temp = xx * xx + yy * yy;
+        result = min(result, temp);
+        return;
+    }
+    for (int i = node; i < N; i++) {
+        if (visited[i]) continue;
+        visited[i] = 1;
+        DFS(cnt + 1,i+1);
+        visited[i] = 0;
+    }
+
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        result = INF;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            visited[i] = 0;
+            cin >> xy[i].first >> xy[i].second;
+        }
+        DFS(0,0);
+        cout << fixed<< setprecision(8);
+        cout<< sqrt(double(result)) << "\n";
+    }
+    return 0;
+}
---
+++ baekjoon/[0913] 무작위_Class5/[17404] RGB 거리 2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int N, dp[1001][3], arr[1001][3], INF = 1000001;
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        dp[i][0] = INF;
+        dp[i][1] = INF;
+        dp[i][2] = INF;
+    }
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+    }
+    for (int n = 0; n < 3; n++) {//N의 RGB값 미리 지정
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+        dp[0][2] = 0;
+        for (int i = 1; i <= N-1; i++) {
+            dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+            if (i == 1||i==N-1)
+                dp[i][n] = INF;//인접 집 색 불가능한 경우 INF처리
+        }
+        dp[N][n] = min(dp[N - 1][2], min(dp[N - 1][0], dp[N - 1][1]))+ arr[N][n];//최종 dp[N][n]
+    }
+    cout << min(dp[N][0], min(dp[N][1], dp[N][2])) << '\n';
+    return 0;
+}
