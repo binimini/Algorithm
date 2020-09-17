87fc743093e420856241dd64398006736dd68efe
Subin Min <tnqls5417@naver.com>
2020-09-17 22:23:14 +0900

    20200915

---
+++ baekjoon/[0915] 무작위_Class5/[12849] 본대 산책.cpp
@@ -0,0 +1,33 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int D, INF = 1000000008, mod = 1000000007;
+int dp[100001][9] = { 0 };//1-8
+vector<int> v[9];
+int main() {
+    cin >> D;
+    v[1] = { 2,3 };
+    v[2] = { 1,3,4 };
+    v[3] = { 1,2,4,5 };
+    v[4] = { 2,3,5,6 };
+    v[5] = { 3,4,6,8 };
+    v[6] = { 4,5,7 };
+    v[7] = { 6,8 };
+    v[8] = { 5,7 };
+    for (int i = 1; i <= D; i++) {
+        for (int j = 1; j <= 8; j++) {
+            dp[i][j] = INF;
+        }
+    }
+    dp[0][1] = 1;
+    for (int i = 1; i <= D; i++) {
+        for (int j = 1; j <= 8; j++) {
+            dp[i][j] = 0;
+            for (int k = 0; k < v[j].size(); k++) {
+                if (dp[i - 1][v[j][k]] >= INF) continue;
+                dp[i][j] = (dp[i][j]+ dp[i - 1][v[j][k]])%mod;
+            }
+        }
+    }
+    cout << dp[D][1]%mod << "\n";
+    return 0;
+}
---
+++ baekjoon/[0915] 무작위_Class5/[12850] 본대 산책 2[FAIL].cpp
@@ -0,0 +1,58 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int D, MOD = 1000000007;
+long long int m[9][9], t[9][9] = { 0 }, to[9][9] = { 0 };
+void mult() {
+    for (int i = 1; i <= 8; i++) {
+        for (int j = 1; j <= 8; j++) {
+            t[i][j] = 0;
+            for (int k = 1; k <= 8; k++) {
+                t[i][j] = (t[i][j] + m[i][k] * m[k][j]) % MOD;
+            }
+        }
+    }
+}
+void odd_mult() {
+    for (int i = 1; i <= 8; i++) {
+        for (int j = 1; j <= 8; j++) {
+            to[i][j] = 0;
+            for (int k = 1; k <= 8; k++) {
+                to[i][j] = (to[i][j] + t[i][k] * m[k][j]) % MOD;
+            }
+        }
+    }
+}
+int main() {
+    cin >> D;
+    m[1][2] = 1, m[1][3] = 1;
+    m[2][1] = 1, m[2][3] = 1, m[2][4] = 1;
+    m[3][1] = 1, m[3][2] = 1, m[3][4] = 1, m[3][5] = 1;
+    m[4][2] = 1, m[4][3] = 1, m[4][5] = 1, m[4][6] = 1;
+    m[5][3] = 1, m[5][4] = 1, m[5][6] = 1, m[5][8] = 1;
+    m[6][4] = 1, m[6][5] = 1, m[6][7] = 1;
+    m[7][6] = 1, m[7][8] = 1;
+    m[8][5] = 1, m[8][7] = 1;
+    while (D != 1) {
+        mult();
+        if (D % 2 == 1) {
+            odd_mult();
+            for (int i = 1; i <= 8; i++) {
+                for (int j = 1; j <= 8; j++) {
+                    m[i][j] = to[i][j];
+                    to[i][j] = 0;
+                }
+            }
+        }
+        else {
+            for (int i = 1; i <= 8; i++) {
+                for (int j = 1; j <= 8; j++) {
+                    m[i][j] = t[i][j];
+                    t[i][j] = 0;
+                }
+            }
+        }
+        D /= 2;
+    }
+    cout << m[1][1] << "\n";
+    return 0;
+}
