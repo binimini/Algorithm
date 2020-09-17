10cc70a2f72ffca600e1f151841e9e34fba48c55
Subin Min <tnqls5417@naver.com>
2020-09-17 22:23:34 +0900

    20200917

---
+++ baekjoon/[0917] 무작위_Class5/[16724] 피리 부는 사나이.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, M, cnt = 0, result = 0;
+char mapp[1000][1000];
+int visited[1000][1000] = { 0 };
+void DFS(int i, int j) {
+    if (visited[i][j]) {
+        if (visited[i][j] < cnt)
+            result--;
+        return;
+    }
+    visited[i][j] = cnt;
+    if (mapp[i][j] == 'U')
+        DFS(i - 1, j);
+    else if (mapp[i][j] == 'D')
+        DFS(i + 1, j);
+    else if (mapp[i][j] == 'L')
+        DFS(i, j - 1);
+    else if (mapp[i][j] == 'R')
+        DFS(i, j + 1);
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> mapp[i][j];
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (visited[i][j]) continue;
+            result++;
+            cnt++;
+            DFS(i,j);
+        }
+    }
+    cout << result << "\n";
+    return 0;
+}
---
+++ baekjoon/[0917] 무작위_Class5/[9527] 1의 개수 세기.cpp
@@ -0,0 +1,34 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+//100011100001101111001001101111110000010000000000000000
+long long int calcu(long long int N) {
+    long long int sum = (N + 1) / 2;
+    int i = 1;
+    long long int cnt = (long long int)(powl(2, i));
+    while (N >= cnt) {
+        if (cnt == N) {
+            sum++;
+            break;
+        }
+        long long int NN = N - (long long int)(powl(2, i));
+        if ((NN / cnt) % 2 != 0) {//홀수 몫
+            sum +=(((NN / cnt) - 1) / 2)* cnt + cnt;
+        }
+        else {//짝수 몫
+            if (NN / cnt != 0)
+                sum += ((NN / cnt) / 2) * cnt + NN % ((NN / cnt) * cnt) + 1;
+            else
+                sum += NN + 1;
+        }
+        i++;
+        cnt = (long long int)(powl(2, i));
+    }
+    return sum;
+}
+int main() {
+    long long int A, B;
+    cin >> A >> B;
+    long long int result = calcu(B) - calcu(A-1);
+    cout << result << "\n";
+    return 0;
+}
