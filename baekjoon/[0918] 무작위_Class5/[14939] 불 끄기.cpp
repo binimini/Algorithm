9eb5e37d4065c1828f3d035022a896a27658891c
Subin Min <tnqls5417@naver.com>
2020-09-18 23:17:14 +0900

    20200918

---
+++ baekjoon/[0918] 무작위_Class5/[14939] 불 끄기.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int cnt = 0, INF = 101, result = INF, adj[5][2] = { {0,0}, {0,-1}, {0,1}, {1,0}, {-1, 0} };
+int map[10] = { 0 }, temp_map[10] = { 0 };
+int main() {
+    char temp;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cin >> temp;
+            if (temp == 'O')
+                map[i] |= (1 << j);
+        }
+    }
+    for (int i = 0; i <= (1 << 10) - 1; i++) {
+        cnt = 0;
+        for (int j = 0; j < 10; j++) {
+            temp_map[j] = map[j];
+        }
+        for (int j = 0; j < 10; j++) {//첫 줄 정하기
+            if (i & (1 << j)) {
+                cnt++;
+                for (int k = 0; k < 5; k++) {
+                    int nx = 0 + adj[k][0];
+                    int ny = j + adj[k][1];
+                    if (nx < 0 || nx>9 || ny < 0 || ny>9) continue;
+                    temp_map[nx] ^= (1 << ny);
+                }
+            }
+        }
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (temp_map[i-1]&(1<<j)){
+                    cnt++;
+                    for (int k = 0; k < 5; k++) {
+                        int ni = i + adj[k][0];
+                        int nj = j + adj[k][1];
+                        if (ni < 0 || ni>9 || nj < 0 || nj>9) continue;
+                        temp_map[ni] ^= (1 << nj);
+                    }
+                }
+            }
+        }
+
+        bool CH = 1;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (temp_map[i] & (1 << j))
+                    CH = 0;
+            }
+        }
+        if (!CH)
+            cnt = INF;
+        result = min(cnt, result);
+    }
+    if (result == INF)
+        cout << -1;
+    else
+        cout << result;
+    return 0;
+}
---
+++ baekjoon/[0918] 무작위_Class5/[16566] 카드 게임.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int N, M, K;
+bool a[4000001] = { 0 };
+int p[4000001];
+vector<int> v;
+int find(int x) {
+    if (p[x] == x)
+        return x;
+    return p[x] = find(p[x]);
+}
+void uni(int x,int y) {
+    x = find(x);//!!!!!!parent 쓰지말고 find() 쓰라고!!!!!!!!
+    y = find(y);
+    if (x < y)
+        p[x] = y;
+    else p[y] = x;
+}
+int upper_bound(int x) {
+    int s = 0, e = N+1;
+    int m;
+    while (e - s > 0) {
+        m = (s + e) / 2;
+        if (m <= x)
+            s = m + 1;
+        else
+            e = m;
+    }
+    if (a[e] == 0) {
+        int ne = find(e);
+        a[ne] = 0;
+        uni(ne, ne + 1);
+        return ne;
+    }
+    else {
+        a[e] = 0;
+        uni(e, e + 1);
+        return e;
+    }
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N >> M >> K;
+    for (int i = 1; i <= N; i++) {
+        p[i] = i;
+    }
+    int temp;
+    for (int i = 0; i < M; i++) {
+        cin >> temp;
+        a[temp] = 1;
+    }
+    int pa = -1;
+    for (int i = N; i >= 1; i--) {
+        if (!a[i]&&pa != -1) {
+            uni(i, pa);
+        }
+        if (a[i])
+            pa = i;
+    }
+    int t;
+    for (int i = 0; i < K; i++) {
+       cin >> t;       
+       cout << upper_bound(t) << "\n";
+    }
+    return 0;
+}
