a34119d179e5ef0d5ce73beaf0f5effbf440421c
Subin Min <tnqls5417@naver.com>
2020-09-20 21:03:06 +0900

    20200919

---
+++ baekjoon/[0919] 무작위_Class6/[11438] LCA 2.cpp
@@ -0,0 +1,65 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i,a,b) for(int i = a; i<b; i++)
+int N, M, MAXP;
+int p[100001][21] = { 0 };
+int level[100001] = { 0 };
+bool visited[100001] = { 0 };
+vector<int> tree[100001];
+void dfs(int node, int depth) {
+    for (int i = 0; i < tree[node].size(); i++) {
+        if (visited[tree[node][i]]) continue;
+        p[tree[node][i]][0] = node;
+        level[tree[node][i]] = depth;
+        visited[tree[node][i]] = 1;
+        dfs(tree[node][i], depth + 1);
+    }
+}
+int lca(int x, int y) {
+    int temp;
+    if (level[y] > level[x]) {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+    for (int i = MAXP; i >= 0; i--) {
+        if (level[x] - level[y] >= (1 << i)) {
+            x = p[x][i];
+        }
+    }
+    if (x == y)
+        return x;
+    for (int i = MAXP; i >= 0; i--) {
+        if (p[x][i] != p[y][i]) {
+            x = p[x][i];
+            y = p[y][i];
+        }
+    }
+    return p[x][0];
+}
+int main() {
+    FASTIO;
+    cin >> N;
+    MAXP = (int)floor(log2(N));
+    int a, b;
+    FOR(i, 0, N-1) {
+        cin >> a >> b;
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+    visited[1] = 1;
+    dfs(1, 1);
+
+    FOR(i, 1, MAXP+1) {
+        FOR(j, 1, N+1) {
+            p[j][i] = p[p[j][i - 1]][i - 1];
+        }
+    }
+    cin >> M;
+    FOR(i, 0, M) {
+        cin >> a >> b;
+        cout <<lca(a, b)<<"\n";
+    }
+    return 0;
+}
---
+++ baekjoon/[0919] 무작위_Class6/[5052] 전화번호 목록.cpp
@@ -0,0 +1,68 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0);
+#define FOR(i,a,b) for(int i = a; i<b; i++)
+struct Trie {//트라이 구조
+    Trie* next[10];
+    bool finish;
+    bool nextchild;
+    Trie() {
+        nextchild = 0;
+        finish = 0;
+        for (int i = 0; i < 10; i++) {
+            next[i] = 0;
+        }
+    }
+    ~Trie() {
+        for (int i = 0; i < 10; i++) {
+            if (next[i]) {
+                delete next[i];
+            }
+        }
+    }
+    void insert(char* s) {
+        if (*s == '\n')
+            finish = true;
+        else {
+            int n = *s - '0';
+            if (!next[n]) next[n] = new Trie;
+            nextchild = true;
+            next[n]->insert(s + 1);
+        }
+    }
+
+    bool consistent() {
+        if (nextchild && finish) return false;
+        for (int i = 0; i < 10; i++) {
+            if (next[i] && !next[i]->consistent()) return false;
+        }
+        return true;
+    }
+};
+int T, N;
+vector<pair<int, int>> v;
+vector<string> s;
+int main() {
+    FASTIO;
+    cin >> T;
+    FOR(t, 0, T) {
+        cin >> N;
+        string temp;
+        Trie* root = new Trie;//루트 포인터??
+        FOR(i, 0, N) {
+            cin >> temp;
+            char* c = new char[temp.size()+1];
+            for (int i = 0; i < temp.size(); i++) {
+                c[i] = temp[i];
+            }
+            c[temp.size()] = '\n';
+            root->insert(c);
+        }
+        if (!root->consistent())
+            cout << "NO\n";
+        else
+            cout << "YES\n";
+        delete root;
+    }
+    return 0;
+}
