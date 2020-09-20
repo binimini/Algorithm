fd4497731d6fd930cc38171efaa5d21ede483788
Subin Min <tnqls5417@naver.com>
2020-09-20 21:21:43 +0900

    20200920

---
+++ baekjoon/그 외 문제/[1008] A 나누기 B.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int A, B;
+int main() {
+    cin >> A >> B;
+    double result = A;
+    result /= B;
+    cout << fixed << setprecision(10);
+    cout << result << "\n";
+    return 0;
+}
