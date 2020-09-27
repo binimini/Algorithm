f3d28f7ab3185f73660d45541933c57a194abd05
Subin Min <tnqls5417@naver.com>
2020-09-27 20:30:17 +0900

    20200925

---
+++ baekjoon/그 외 문제/[9161] Sir Bedavere’s Bogus Division Solutions.cpp
@@ -0,0 +1,19 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int ni, ci, nj, cj;
+    for (int i = 100; i <= 999; i++) {
+        for (int j = 100; j <= 999; j++) {
+            if (i == j)
+                continue;
+            ni = i / 10;
+            ci = i % 10;
+            nj = j % 100;
+            cj = j / 100;
+            if (ci==cj&&double(i) / j == double(ni) / nj) {
+                cout << i << " / " << j << " = " << ni << " / " << nj << "\n";
+            }
+        }
+    }
+    return 0;
+}
