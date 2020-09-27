cf14bc9f0bf83e8bb589b4468463ef77ab1c841d
Subin Min <tnqls5417@naver.com>
2020-09-27 20:28:28 +0900

    20200927

---
+++ baekjoon/[0927] 무작위_Gold5/[13023] ABCDE.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+#define FOR(i,a,b) for(int i = a; i<b; i++)
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0);
+int N, M;
+bool FLAG = 0;
+vector<int> edge[2000];
+bool visited[2000] = { 0 };
+void dfs(int node, int cnt) {
+    if (cnt >= 5) {
+        FLAG = 1;
+        return;
+    }
+    FOR(i, 0, edge[node].size()) {
+        if (visited[edge[node][i]]) continue;
+        visited[edge[node][i]] = 1;
+        dfs(edge[node][i], cnt + 1);
+        visited[edge[node][i]] = 0;//사이클 돌아서 가지 않을 수 있으므로 백트래킹 필요
+    }
+}
+int main() {
+    FASTIO;
+    cin >> N >> M;
+    int a, b;
+    FOR(i, 0, M) {
+        cin >> a >> b;
+        edge[a].push_back(b);
+        edge[b].push_back(a);
+    }
+    FOR(i, 0, N) {
+        if (FLAG)
+            break;
+        FOR(j, 0, N) {
+            visited[j] = 0;
+        }
+        visited[i] = 1;
+        dfs(i, 1);
+    }
+    cout << FLAG;
+    return 0;
+}
---
+++ baekjoon/[0927] 무작위_Gold5/[3020] 개똥벌레.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#define FOR(i,a,b) for(int i = a; i<b; i++)
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0);
+int N, H;
+vector<int> l;
+vector<int> u;
+int lower_bound(int key) {
+    int s = 0, e = l.size(), m;
+    while (e - s > 0) {
+        m = (s + e) / 2;
+        if (key > l[m])
+            s = m + 1;
+        else
+            e = m;
+    }
+    return e;
+}
+int upper_bound(int key) {
+    int s = 0, e = u.size(), m;
+    while (e - s > 0) {
+        m = (s + e) / 2;
+        if (key >= u[m])
+            s = m + 1;
+        else
+            e = m;
+    }
+    return e;
+}
+int main() {
+    FASTIO;
+    cin >> N >> H;
+    int temp;
+    FOR(i, 0, N) {
+        cin >> temp;
+        if (i % 2 == 0)
+            u.push_back(temp);
+        else
+            l.push_back(temp);
+    }
+    sort(l.begin(), l.end());
+    sort(u.begin(), u.end());
+    pair<int, int> mini = { 200001, 0 };
+    int sum;
+    FOR(i, 1, H + 1) {
+        sum = 0;
+        sum += l.size() - lower_bound(i);
+        sum += u.size()- upper_bound(H-i);
+        if (mini.first >= sum) {
+            if (mini.first == sum)
+                mini.second++;
+            else {
+                mini.first = sum;
+                mini.second = 1;
+            }
+        }
+    }
+    cout << mini.first << " " << mini.second << "\n";
+    return 0;
+}
