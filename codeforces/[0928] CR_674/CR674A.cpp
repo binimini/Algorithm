954af3558a3c2dc0a035cc4c446e3c1cfa90440d
Subin Min <tnqls5417@naver.com>
2020-09-28 22:41:52 +0900

    20200928 CR_674[A,B]

---
+++ codeforces/[0928] CR_674/CR674A.cpp
@@ -0,0 +1,20 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+int T, N, X;
+int main() {
+    FASTIO;
+    cin >> T;
+    FOR(t, 0, T) {
+        cin >> N >> X;
+        if (N == 1 || N == 2)
+            cout << 1 << "\n";
+        else if (N == 3)
+            cout << 2 << "\n";
+        else {
+            cout << (N - 3) / X + 2 << "\n";
+        }
+    }
+    return 0;
+}
---
+++ codeforces/[0928] CR_674/CR674B.cpp
@@ -0,0 +1,27 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+int T, N, M;
+int main() {
+    FASTIO;
+    cin >> T;
+    FOR(t, 0, T) {
+        cin >> N >> M;
+        bool right = 0;
+        int tl, tr, bl, br;
+        FOR(i, 0, N) {
+            cin >> tl >> tr;
+            cin >> bl >> br;
+            if (bl == tr)
+                right = 1;
+        }
+        if (M % 2 != 0)
+            cout << "NO\n";
+        else if (right)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
