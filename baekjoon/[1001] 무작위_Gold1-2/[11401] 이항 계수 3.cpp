ac34069d37bcdf545f66e931a20ff3d2a318b796
Subin Min <tnqls5417@naver.com>
2020-10-01 22:17:05 +0900

    20201001

---
+++ baekjoon/[1001] 무작위_Gold1-2/[11401] 이항 계수 3.cpp
@@ -0,0 +1,49 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int mod = 1000000007;
+int N, K;
+//4000000의 nCk 4000000을 반복해서 곱하므로 longlongint 범위 벗어날 수 있음
+//나누기, 나머지에 대한 모듈러 연산 불가능 -> (n-k)!k! 나누기 연산 불가
+// -> (n-k)!k!의 역원을 구해 곱한다. (곱에 대한 모듈러 연산 가능)
+
+//페르마의 소정리 a^p =(합동) a (mod p) a^(p-1) =(합동) 1 (mod p)
+// -> a*a^(p-2) =(합동) 1 (mod p) -> a의 역원 a^(p-2) a에 (n-k)!k! 대입
+
+//1000000005 제곱 O(N)시 시간 초과 -> 분할 정복을 통한 제곱 -> O(logN)
+
+long long int divided_pow(int a, int n) {
+    if (n == 0)
+        return 1;
+    long long int b;
+    if (n % 2 == 0) {
+        b = divided_pow(a, n / 2);
+        b = (b * b) % mod;
+        return b;
+    }
+    else if (n % 2 != 0) {
+        b = divided_pow(a, (n - 1) / 2);
+        b = (b * b) % mod;
+        b = (b * a) % mod;
+        return b;
+    }
+}
+int main() {
+    long long int result = 1;
+    cin >> N >> K;
+    for (int i = 1; i <= N; i++) {
+        result = (result * i) % mod;
+    }
+    
+    long long int forinverse = 1;
+    for (int i = 1; i <= N - K; i++) {
+        forinverse = (forinverse * i) % mod;
+    }
+    for (int i = 1; i <= K; i++) {
+        forinverse = (forinverse * i) % mod;
+    }
+    long long int inverse;
+    inverse = divided_pow(forinverse, mod - 2);
+    
+    cout << (result * inverse) % mod;
+    return 0;
+}
---
+++ baekjoon/[1001] 무작위_Gold1-2/[2749] 피보나치 수 3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int dp[1500001];
+long long int N;
+int fibbo(int i) {
+    if (dp[i] != -1)
+        return dp[i];
+    else
+        return dp[i] = (fibbo(i - 1) + fibbo(i - 2))%1000000;
+}
+int main() {
+    cin >> N;
+    for (int i = 1; i <= 1500000; i++) {
+        dp[i] = -1;
+    }
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    for (int i = 3; i <= 1500000; i++) {
+        fibbo(i);
+    }
+    cout << fibbo(N % 1500000) << "\n";
+    return 0;
+}
