9ddebcbee94c55be5e4c4ed13ae45545fe09e7fc
Subin Min <tnqls5417@naver.com>
2020-10-02 22:55:00 +0900

    20201001 exception(repetitve checking -> MLE) handling

--- baekjoon/[0921] 무작위_Class6/[5719] 거의 최단 경로.cpp
+++ baekjoon/[0921] 무작위_Class6/[5719] 거의 최단 경로.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int T, N, M, S, D, INF = 500001, CNT = 1;
 vector<pair<int,int>> edge[500];
 pair<int,vector<int>> dist[500];
+int visited[500] = { 0 };
 int cant_use[500][500] = { 0 };
 priority_queue<pair<int, int>> pq;
 queue<int> q;
@@ -23,10 +24,11 @@ void dijkstra() {
                 if (vc + nc < dist[n].first) {
                     dist[n].second.clear();
                     dist[n].first = vc + nc;
+                    pq.push(make_pair(-dist[n].first, n));//같은 거리값으로 들어왔으면 pq에 새로 넣을 필요 X //경로에만 추가
 
                 }
                 dist[n].second.push_back(v);
-                pq.push(make_pair(-dist[n].first, n));
+
             }
         }
     }
@@ -55,6 +57,8 @@ int main() {
         while (!q.empty()) {
             int x = q.front();
             q.pop();
+            if (visited[x] == CNT) continue;//이미 체크한 x의 사용한 경로 반복체크 없애는 용도
+            visited[x] = CNT;
             FOR(i, 0, dist[x].second.size()) {
                 cant_use[dist[x].second[i]][x] = CNT;
                 if (dist[x].second[i] == S) continue;
