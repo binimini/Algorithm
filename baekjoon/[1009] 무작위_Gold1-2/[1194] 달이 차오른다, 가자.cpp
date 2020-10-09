9c3433806524309dad3904cbe1ec9d07b94c3815
Subin Min <tnqls5417@naver.com>
2020-10-09 21:02:14 +0900

    20201009

---
+++ baekjoon/[1009] 무작위_Gold1-2/[1194] 달이 차오른다, 가자.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int N, M, R = 987654321;
+bool flag;
+char map[51][51];
+int visited[50][50][64] = { 0 }, d[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
+queue<pair<pair<int, int>, int>> q;
+void bfs() {
+    while (!q.empty()) {
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int s = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int nx = x + d[i][0];
+            int ny = y + d[i][1];
+            if (nx<0 || nx>N - 1 || ny<0 || ny>M - 1) continue;
+            if (map[nx][ny] == '#') continue;//벽
+            if (visited[nx][ny][s]) continue;
+            visited[nx][ny][s] = visited[x][y][s]+1;
+            if (map[nx][ny] == '1') {//출구
+                R = visited[nx][ny][s];
+                flag = 1;
+                return;
+            }
+            else if (map[nx][ny] >= 'A' && map[nx][ny] <= 'Z') {//문
+                if ((1 << (map[nx][ny] - 'A')) & s) {
+                    q.push({ {nx,ny},s });
+                }
+                else continue;
+            }
+            else if (map[nx][ny] >= 'a' && map[nx][ny] <= 'z') {//열쇠
+                visited[nx][ny][s | (1 << (map[nx][ny] - 'a'))] = visited[nx][ny][s];
+                q.push({ { nx,ny }, s | (1 << (map[nx][ny] - 'a')) });
+            }
+            else if (map[nx][ny] == '.')
+                q.push({ {nx,ny},s });
+        }
+    }
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> map[i][j];
+            if (map[i][j] == '0') {
+                q.push({ { i,j },0 });
+                map[i][j] = '.';
+                visited[i][j][0] = 1;
+            }
+        }
+    }
+    bfs();
+    if (flag)
+        cout << R-1;
+    else
+        cout << -1;
+    return 0;
+}
---
+++ baekjoon/[1009] 무작위_Gold1-2/[1655] 가운데를 말해요.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int N, middle = 10001;
+priority_queue<int> g;
+priority_queue<int> l;
+int check(int i) {
+    if (i % 2 == 0) {
+        if (l.size() + 1 == g.size())
+            return 1;
+        else if (l.size() + 1 > g.size())
+            return 0;
+        else if (l.size() + 1 < g.size())
+            return -1;
+    }
+    else {
+        if (l.size() == g.size())
+            return 1;
+        else if (l.size() > g.size())
+            return 0;
+        else if (l.size() < g.size())
+            return -1;
+    }
+
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    int temp;
+    for (int i = 1; i <= N; i++) {
+        cin >> temp;
+        if (middle == 10001)
+            middle = temp;
+        else {
+            if (temp == middle) {
+                if (g.size() <= l.size())
+                    g.push(-temp);
+                else
+                    l.push(temp);
+            }
+            else if (temp > middle)
+                g.push(-temp);
+            else
+                l.push(temp);
+            int flag = check(i);
+            if (flag == 0) {
+                g.push(-middle);
+                middle = l.top();
+                l.pop();
+            }
+            else if (flag == -1) {
+                l.push(middle);
+                middle = -g.top();
+                g.pop();
+            }
+        }
+        cout << middle << "\n";
+    }
+    return 0;
+}
