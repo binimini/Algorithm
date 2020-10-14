dd4142783560c18abc8906962f3fd460e3cc17ed
Subin Min <tnqls5417@naver.com>
2020-10-14 22:04:38 +0900

    20201014

---
+++ baekjoon/[1014] 무작위_Gold1-2/[12738] 가장 긴 증가하는 부분 수열 3.cpp
@@ -0,0 +1,34 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N;
+vector<int> v;
+int lb(int k) {
+    int s = 0, e = v.size(), m;
+    while (e - s > 0) {
+        m = (s + e) / 2;
+        if (v[m] < k)
+            s = m + 1;
+        else
+            e = m;
+    }
+    return e;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    int temp;
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        if (v.empty()) 
+            v.push_back(temp);
+        else {
+            if (v.back() < temp) 
+                v.push_back(temp);
+            else 
+                v[lb(temp)] = temp;
+        }
+    }
+    cout << v.size();
+    return 0;
+}
---
+++ baekjoon/[1014] 무작위_Gold1-2/[1328] 고층 빌딩.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, L, R, MOD = 1000000007;
+int dp[101][101][101];
+long long int find_dp(int n, int l, int r) {
+    if ((l == 1 && r == n) || (r == 1 && l == n))
+        return 1;
+    if (n == 0 || l == 0 || r == 0)
+        return 0;
+    if (dp[n][l][r] != -1) 
+        return dp[n][l][r];
+    
+    int& result = dp[n][l][r];
+    long long int n_ = n;
+    n_ -= 2;
+    return result = (find_dp(n - 1, l - 1, r) + find_dp(n - 1, l, r - 1) + n_ * find_dp(n - 1, l, r)) % MOD;
+}
+int main() {
+    cin >> N>>L>>R;
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j <= N; j++) {
+            for (int k = 0; k <= N; k++) {
+                dp[i][j][k] = -1;
+            }
+        }
+    }
+    cout << find_dp(N, L, R);
+    return 0;
+}
