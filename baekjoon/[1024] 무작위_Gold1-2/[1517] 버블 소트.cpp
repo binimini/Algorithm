eca685ce7dc7556395362038cac91f299fc261e8
Subin Min <tnqls5417@naver.com>
2020-10-24 23:00:42 +0900

    20201024

---
+++ baekjoon/[1024] 무작위_Gold1-2/[1517] 버블 소트.cpp
@@ -0,0 +1,42 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+int N;
+pair<int, int> arr[500000];
+long long int tree[2000001] = { 0 };
+void update(int node, int start, int end, int index, long long int diff) {//node tree 번호(1부터시작) start, end, index(arr, 0부터시작), diff 차이값 
+    if (!(start <= index && index <= end))
+        return;
+    tree[node] += diff;
+    if (start == end)
+        return;
+    int mid = (start + end) / 2;
+    update(node * 2, start, mid, index, diff);
+    update(node * 2+1, mid+1, end, index, diff);
+}
+long long int sum(int node, int start, int end, int left, int right) {//left right 구하고자하는 범위
+    if (start > right || end < left)
+        return 0;
+    if (left <= start && end <= right)
+        return tree[node];
+        
+    int mid = (start + end) / 2;
+    return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid + 1, end, left, right);
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    FOR(i, 0, N) {
+        cin >> arr[i].first;
+        arr[i].second = i;
+    }
+    sort(arr, arr + N);
+    long long int cnt = 0;
+    FOR(i, 0, N) {
+        cnt += sum(1, 0, N - 1, arr[i].second, N - 1);
+        update(1, 0, N - 1, arr[i].second, 1);
+    }
+    cout << cnt;
+    return 0;
+}
---
+++ baekjoon/[1024] 무작위_Gold1-2/[9576] 책 나눠주기.cpp
@@ -0,0 +1,48 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for (int i = a; i<b; i++)
+int T, N, M;
+vector<bool> visited;//초기화하고 i번째 기준으로 dfs 시킬때 각 dfs(i) 방문여부
+vector<int> A, B;//A 매칭 노드 B 매칭 노드 표시 //이어나감
+vector<int> e[1001];
+bool dfs(int a) {//이분 매칭
+    if (visited[a]) return false;//아미 처리한 것일 경우 지나감???
+    visited[a] = true;
+    for (int b = 0; b < e[a].size(); b++) {
+        if (B[e[a][b]] == -1 || dfs(B[e[a][b]])) {//연결되지 않았거나 연결되었던 A가 다른 매칭 가능한 경우
+            A[a] = e[a][b];
+            B[e[a][b]] = a;
+
+            return true;
+        }
+    }
+    return false;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> T;
+    FOR(t, 0, T) {
+        cin >> N >> M;
+        int a, b;
+        for (int i = 1; i <= M; i++) {
+            e[i].clear();
+            cin >> a >> b;
+            for (int j = a; j <= b; j++) {
+                e[i].push_back(j);
+            }
+        }
+        A = vector<int>(M+1, -1);
+        B = vector<int>(N+1, -1);
+        int cnt = 0;
+        for (int i = 1; i <= M; i++) {
+            visited = vector<bool>(M+1, false);
+            if (dfs(i))
+                cnt++;
+        }
+        cout << cnt << "\n";
+    }
+
+    return 0;
+}
