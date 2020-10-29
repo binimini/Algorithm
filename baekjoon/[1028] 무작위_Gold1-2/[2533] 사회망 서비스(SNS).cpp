6eb47fce5cc15016f76f89c6dd5ab25dd34be395
Subin Min <tnqls5417@naver.com>
2020-10-29 22:30:42 +0900

    20201028

---
+++ baekjoon/[1028] 무작위_Gold1-2/[2533] 사회망 서비스(SNS).cpp
@@ -0,0 +1,55 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N,dp[1000001][2];
+bool visited[1000001] = { 0 };
+vector<int> edge[1000001];
+int find_dp(int node, bool early) {
+    if (dp[node][early] != -1)
+        return dp[node][early];
+    visited[node] = 1;
+    int& r = dp[node][early];
+    r = 0;
+    bool flag = 1;
+    for (int i = 0; i < edge[node].size(); i++) {
+        if (!visited[edge[node][i]]) {
+            flag = 0;
+            break;
+        }
+    }
+    if (flag) {
+        visited[node] = 0;
+        if (early)
+            return r = 1;
+        else
+            return r = 0;
+    }
+    if (!early) {
+        for (int i = 0; i < edge[node].size(); i++) {
+            if (visited[edge[node][i]]) continue;
+            r += find_dp(edge[node][i], 1);
+        }
+    }
+    else {
+        for (int i = 0; i < edge[node].size(); i++) {
+            if (visited[edge[node][i]]) continue;
+            r += min(find_dp(edge[node][i], 0), find_dp(edge[node][i], 1));
+        }
+        r += 1;
+    }
+    visited[node] = 0;
+    return r;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    memset(dp, -1, sizeof(dp));
+    cin >> N;
+    int u, v;
+    for (int i = 1; i < N; i++) {
+        cin >> u >> v;
+        edge[u].push_back(v);
+        edge[v].push_back(u);
+    }
+    cout << min(find_dp(1,0), find_dp(1,1));
+    return 0;
+}
---
+++ baekjoon/[1028] 무작위_Gold1-2/[3197] 백조의 호수.cpp
@@ -0,0 +1,116 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int R, C, d[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
+char lake[1500][1500];
+int ice[1500][1500] = { 0 };
+bool v[1500][1500] = { 0 };
+vector<pair<int,int>> water;
+pair<int, int> s, e;
+queue<pair<int, int>> q;
+queue < pair<int, int>> sw;
+bool Swan(int k) {
+    sw.push(s);
+    v[s.first][s.second] = 1;
+    while (!sw.empty()) {
+        int x = sw.front().first;
+        int y = sw.front().second;
+        sw.pop();
+        for (int i = 0; i < 4; i++) {
+            int nx = x + d[i][0];
+            int ny = y + d[i][1];
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+            if (lake[nx][ny] == 'X' && ice[nx][ny] > k + 1) {
+                continue;
+            }
+            if (v[nx][ny]) continue;
+            if (nx == e.first && ny == e.second) {
+                while (!sw.empty()) {
+                    sw.pop();
+                }
+                return true;
+            }
+            v[nx][ny] = 1;
+            sw.push(make_pair(nx, ny));
+        }
+    }
+    return false;
+}
+void Water() {
+    while (!water.empty()) {
+        int x = water.back().first;
+        int y = water.back().second;
+        water.pop_back();
+        ice[x][y] = 1;
+        bool flag = 0;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + d[i][0];
+            int ny = y + d[i][1];
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+            if (ice[nx][ny]) continue;
+            if (lake[nx][ny] == 'X') {
+                ice[nx][ny] = ice[x][y]+1;//첫 X 2
+                q.push(make_pair(nx, ny));
+            }
+        }
+    }
+}
+void bfs() {
+    int cnt = 1;
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int nx = x + d[i][0];
+            int ny = y + d[i][1];
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+            if (ice[nx][ny]) continue;
+            if (lake[nx][ny] == 'X') {
+                ice[nx][ny] = ice[x][y] + 1;
+                q.push(make_pair(nx, ny));
+            }
+        }
+    }
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> R >> C;
+    bool flag = 0;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> lake[i][j];
+            if (!flag && lake[i][j] == 'L') {
+                s = { i,j };
+                lake[i][j] = '.';
+                flag = 1;
+            }
+            else if (flag && lake[i][j] == 'L') {
+                e = { i,j };
+                lake[i][j] = '.';
+            }
+            if (lake[i][j] == '.')
+                water.push_back(make_pair(i, j));
+        }
+    }
+    Water();
+    bfs();
+    int l = 0, r = 3000, m, cnt = 3000;
+    while (r - l >= 0) {
+        m = (r + l) / 2;
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (v[i][j]) v[i][j] = 0;
+            }
+        }
+        if (Swan(m)) {
+            cnt = min(cnt, m);
+            r = m - 1;
+        }
+        else {
+            l = m + 1;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
