c2ca8285fa5ffdc5c03a2386bb803d81bf1fdba0
Subin Min <tnqls5417@naver.com>
2020-10-31 21:12:43 +0900

    20201031

---
+++ baekjoon/[1031] 무작위_Gold1-2/[2629] 양팔저울.cpp
@@ -0,0 +1,40 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, M, arr[31];
+bool dp[31][16001] = { 0 };
+void solution(int n, int value) {
+    if (n > N||value>15000||value<0)
+        return;
+    if (dp[n][value]) 
+        return;
+    dp[n][value] = 1;
+    solution(n + 1, value + arr[n + 1]);
+    solution(n + 1, abs(value - arr[n + 1]));
+    solution(n + 1, value);
+}
+int main() {
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+    solution(0,0);
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        int temp;
+        bool flag = 0;
+        cin >> temp;
+        if (temp <= 15000) {
+            for (int i = 0; i <= N; i++) {
+                if (dp[i][temp]) {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if (flag)
+            cout << "Y ";
+        else 
+            cout << "N ";
+    }
+    return 0;
+}
---
+++ baekjoon/[1031] 무작위_Gold1-2/[3015] 오아시스 재결합.cpp
@@ -0,0 +1,46 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+stack<pair<int,int>> s;
+int N;
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    int temp, temp_cnt = 1;
+    long long int cnt = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        temp_cnt = 1;
+        if (s.empty())
+            s.push(make_pair(temp,1));
+        else if (s.top().first > temp) {
+            s.push(make_pair(temp, 1));
+            cnt++;
+        }
+        else if (s.top().first <= temp) {
+            while (!s.empty() && s.top().first < temp) {
+                cnt += s.top().second;
+                s.pop();
+            }
+            if (!s.empty()) {
+                if (s.top().first == temp) {
+                    pair<int, int> temp_p = s.top();
+                    cnt += temp_p.second;
+                    s.pop();
+                    if (!s.empty())
+                        cnt++;
+                    temp_p.second++;
+                    s.push(temp_p);
+                }
+                else {
+                    cnt++;
+                    s.push(make_pair(temp, 1));
+                }
+            }
+            else
+                s.push(make_pair(temp, 1));
+        }
+    }
+    cout << cnt << "\n";
+    return 0;
+}
