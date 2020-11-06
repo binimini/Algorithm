50b261638ba7d63fcc51b075dd24824db6212c13
Subin Min <tnqls5417@naver.com>
2020-11-06 22:11:57 +0900

    20201106

---
+++ baekjoon/[1106] 무작위_Gold1-2/[1103] 게임.cpp
@@ -0,0 +1,50 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, M;
+int board[50][50], d[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
+int dp[50][50];
+bool visited[50][50] = { 0 }, flag = 0;
+int solution(int n, int m) {
+    if (n<0 || n>N - 1 || m<0 || m>M - 1)
+        return 0;
+    if (board[n][m] == 0)
+        return 0;
+    if (visited[n][m]) {
+        flag = 1;
+        return 0;
+    }
+    if (dp[n][m] != -1)
+        return dp[n][m];
+    int& r = dp[n][m];
+    visited[n][m] = 1;
+    r = 0;
+    for (int i = 0; i < 4; i++) {
+        int nx = n + d[i][0] * board[n][m];
+        int ny = m + d[i][1] * board[n][m];
+        r = max(r, solution(nx, ny) + 1);
+    }
+    visited[n][m] = 0;
+    return r;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    char temp;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> temp;
+            if (temp == 'H')
+                board[i][j] = 0;
+            else
+                board[i][j] = temp - '0';
+        }
+    }
+    memset(dp, -1, sizeof(dp));
+    int result = solution(0, 0);
+    if (flag)
+        cout << -1;
+    else
+        cout << result;
+    return 0;
+}
---
+++ baekjoon/[1106] 무작위_Gold1-2/[1208] 부분수열의 합 2.cpp
@@ -0,0 +1,60 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N,S,arr[40];
+vector<int> A, B;
+long long int cnt = 0;
+void dfsA(int n,int sum) {
+    if (n == N / 2) {
+        A.push_back(sum);
+        return;
+    }
+    dfsA(n + 1, sum + arr[n]);
+    dfsA(n + 1, sum);
+}
+void dfsB(int n, int sum) {
+    if (n == N) {
+        B.push_back(sum);
+        return;
+    }
+    dfsB(n + 1, sum + arr[n]);
+    dfsB(n + 1, sum);
+}
+int lb(int k) {
+    int s = 0, e = A.size(), m;
+    while (e - s > 0) {
+        m = (s + e) / 2;
+        if (A[m] < k)
+            s = m + 1;
+        else
+            e = m;
+    }
+    return e;
+}
+int ub(int k) {
+    int s = 0, e = A.size(), m;
+    while (e - s > 0) {
+        m = (s + e) / 2;
+        if (A[m] <= k)
+            s = m + 1;
+        else
+            e = m;
+    }
+    return e;
+}
+int main() {
+    cin >> N >>S;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    dfsA(0,0);
+    dfsB(N/2,0);
+    sort(A.begin(), A.end());
+    for (int i = 0; i < B.size(); i++) {
+        cnt += ub(S - B[i]);
+        cnt -= lb(S - B[i]);
+    }
+    if (S == 0)
+        cnt--;
+    cout << cnt;
+    return 0;
+}
