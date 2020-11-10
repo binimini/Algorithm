c6b8580875986cb9f5d302d504ddb5ddb69fae4b
Subin Min <tnqls5417@naver.com>
2020-11-10 21:54:27 +0900

    20201110

---
+++ baekjoon/[1110] 무작위_Gold1-2/[2610] 회의준비.cpp
@@ -0,0 +1,87 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, M, INF = 1000;
+int dist[101], p[101], m[101];
+vector<int> e[101];
+pair<int, int> result[101];
+priority_queue<pair<int, int>> pq;
+int find_p(int x) {
+    if (p[x] == x)
+        return x;
+    else
+        return p[x] = find_p(p[x]);
+}
+void union_p(int x, int y) {
+    x = find_p(x);
+    y = find_p(y);
+    if (x < y)
+        p[y] = x;
+    else
+        p[x] = y;
+}
+void dijkstra(int n) {
+    for (int i = 1; i <= N; i++)
+        dist[i] = INF;
+    dist[n] = 0;
+    pq.push(make_pair(0, n));
+    while (!pq.empty()) {
+        int xc = -pq.top().first;
+        int x = pq.top().second;
+        pq.pop();
+        if (dist[x] < xc) continue;
+        for (int i = 0; i < e[x].size(); i++) {
+            int n = e[x][i];
+            int nc = xc + 1;
+            if (dist[n] > nc) {
+                dist[n] = nc;
+                pq.push(make_pair(-nc, n));
+            }
+        }
+    }
+    
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        p[i] = i;
+        result[i].first = INF;
+    }
+    int a, b;
+    for (int i = 0; i < M; i++) {
+        cin >> a >> b;
+        e[a].push_back(b);
+        e[b].push_back(a);
+        union_p(a, b);
+    }
+    for (int i = 1; i <= N; i++) {
+        dijkstra(i);
+        int mm = -1;
+        for (int j = 1; j <= N; j++) {
+            if (dist[j] != INF)
+                mm = max(mm, dist[j]);
+        }
+        m[i] = mm;
+    }
+    for (int i = 1; i <= N; i++) {
+        int pa = find_p(i);
+        if (result[pa].first > m[i]) {
+            result[pa].first = m[i];
+            result[pa].second = i;
+        }
+    }
+    vector<int> r;
+    for (int i = 1; i <= N; i++) {
+        if (result[i].first != INF)
+            r.push_back(result[i].second);
+    }
+    sort(r.begin(), r.end(), greater<int>());
+    cout << r.size() << "\n";
+    while(!r.empty()){
+        cout << r.back() << "\n";
+        r.pop_back();
+    }
+    return 0;
+}
---
+++ baekjoon/[1110] 무작위_Gold1-2/[5676] 음주 코딩.cpp
@@ -0,0 +1,88 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int T,N,K;
+int tree[400004], arr[100001];
+vector<int> result;
+int init(int node, int start, int end) {
+    if (start == end) {
+        return tree[node] = arr[start];
+    }
+    int mid = (start + end) / 2;
+    tree[node] = init(node * 2, start, mid) * init(node * 2 + 1, mid + 1, end);
+    return tree[node];
+}
+void update(int node, int start, int end, int idx,int pre, int to) {
+    if (!(start <= idx && idx <= end))
+        return;
+    if (pre == 0) {//arr[idx] 바꿔놓은 후에
+        init(node, start, end);
+    }
+    else {
+        if (pre == -1 && to == 1)
+            tree[node] *= -1;
+        else
+            tree[node] *= to;
+        if (start != end) {
+            int mid = (start + end) / 2;
+            update(node * 2, start, mid, idx, pre, to);
+            update(node * 2 + 1, mid + 1, end, idx, pre, to);
+        }
+    }
+}
+int query(int node, int start, int end, int left, int right) {
+    if (right < start || left > end) {
+        return 1;
+    }
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+    int mid = (start + end) / 2;
+    return query(node * 2, start, mid, left, right) * query(node * 2 + 1, mid + 1, end, left, right);
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    while(cin >>N>>K){
+        result.clear();
+        for (int i = 1; i <= N; i++) {
+            cin >> arr[i];
+            if (arr[i] > 0)
+                arr[i] = 1;
+            else if (arr[i] < 0)
+                arr[i] = -1;
+            else
+                arr[i] = 0;
+        }
+        init(1, 1, N);
+        char comm;
+        int a, b;
+        for (int i = 0; i < K; i++) {
+            cin >> comm >> a >> b;
+            if (comm == 'C') {
+                if (b > 0)
+                    b = 1;
+                else if (b < 0)
+                    b = -1;
+                int temp = arr[a];
+                arr[a] = b;
+                if (temp == arr[a])
+                    continue;
+                update(1, 1, N, a, temp, arr[a]);
+            }
+            else if (comm == 'P') {
+                result.push_back(query(1, 1, N, a, b));
+            }
+        }
+        for (int i = 0; i < result.size(); i++) {
+            if (result[i] == 1)
+                cout << '+';
+            else if (result[i] == -1)
+                cout << '-';
+            else
+                cout << '0';
+        }
+        cout << "\n";
+    }
+    return 0;
+}
