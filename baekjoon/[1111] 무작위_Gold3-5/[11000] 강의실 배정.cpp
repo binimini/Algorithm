725f8e9cfbd0609a70b96eb87104b01b27be1fe7
Subin Min <tnqls5417@naver.com>
2020-11-11 22:31:56 +0900

    20201111

---
+++ baekjoon/[1111] 무작위_Gold3-5/[11000] 강의실 배정.cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+priority_queue<int> pq;
+int N, result = 0;
+pair<int,int> arr[200000];
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i].first>>arr[i].second;
+    }
+    sort(arr, arr + N);
+    int cnt = 0;
+    while (cnt<=N-1) {
+        if (pq.empty()) {
+            pq.push(-arr[cnt].second);
+        }
+        else {
+            while (!pq.empty()&&-pq.top() <= arr[cnt].first) {
+                pq.pop();
+            }
+            pq.push(-arr[cnt].second);
+        }
+        cnt++;
+        int size = pq.size();
+        result = max(result, size);
+    }
+    cout << result;
+    return 0;
+}
---
+++ baekjoon/[1111] 무작위_Gold3-5/[17298] 오큰수.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, arr[1000000],ans[1000000];
+stack<int> s;
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    for (int i = N - 1; i >= 0; i--) {
+        while (!s.empty() && arr[i] >= s.top()) {
+            s.pop();
+        }
+        if (s.empty())
+            ans[i] = -1;
+        else
+            ans[i] = s.top();
+        s.push(arr[i]);
+    }
+    for (int i = 0; i < N; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
