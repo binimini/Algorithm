0872f74abc5cadb71326d6e327624328da94338b
Subin Min <tnqls5417@naver.com>
2020-11-12 19:53:31 +0900

    20201112

---
+++ baekjoon/[1112] 무작위_Gold1-2/[2211] 네트워크 복구.cpp
@@ -0,0 +1,47 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, M, INF = 10001;
+vector<pair<int,int>> e[1001];
+pair<int, int> dist[1001];
+priority_queue<pair<int, int>> pq;
+bool restore[1001][1001] = { 0 };
+void dijkstra() {
+    for (int i = 1; i <= N; i++) {
+        dist[i].first = INF;
+    }
+    dist[1].first = 0, dist[1].second = { -1 };
+    pq.push(make_pair(0, 1));
+    while (!pq.empty()) {
+        int xc = -pq.top().first;
+        int x = pq.top().second;
+        pq.pop();
+        if (xc > dist[x].first) continue;
+        for (int i = 0; i < e[x].size(); i++) {
+            int n = e[x][i].first;
+            int nc = xc + e[x][i].second;
+            if (nc < dist[n].first) {
+                dist[n].first = nc;
+                dist[n].second = x;
+                pq.push(make_pair(-nc, n));
+            }
+        }
+    }
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N >> M;
+    int a, b, c;
+    for (int i = 0; i < M; i++) {
+        cin >> a >> b >> c;
+        e[a].push_back(make_pair(b, c));
+        e[b].push_back(make_pair(a, c));
+    }
+    dijkstra();
+    cout << N - 1 << "\n";
+    for (int i = 2; i <= N; i++) {
+        cout << dist[i].second <<" " << i << "\n";
+    }
+    return 0;
+}
---
+++ baekjoon/[1112] 무작위_Gold1-2/[2268] 수들의 합.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+long long int tree[4000004] = { 0 };
+int arr[1000001] = { 0 };
+int N, M;
+void update(int node, int start, int end, int idx, int diff) {
+    if (!(start <= idx && idx <= end))
+        return;
+    tree[node] += diff;
+    if (start != end) {
+        int mid = (start + end) / 2;
+        update(node * 2, start, mid, idx, diff);
+        update(node * 2+1, mid+1, end, idx, diff);
+    }
+}
+ll sum(int node, int start, int end, int left, int right) {
+    if (end < left || right < start)
+        return 0;
+    if (left <= start && end <= right)
+        return tree[node];
+    int mid = (start + end) / 2;
+    return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid + 1, end, left, right);
+}
+int main() {
+    FASTIO;
+    cin >> N >> M;
+    int a, b, c;
+    for (int i = 0; i < M; i++) {
+        cin >> a >> b >> c;
+        if (a) {
+            update(1, 1, N, b, c - arr[b]);
+            arr[b] = c;
+        }
+        else {
+            cout << sum(1, 1, N, min(b,c), max(b,c)) << "\n";
+        }
+    }
+    return 0;
+}
