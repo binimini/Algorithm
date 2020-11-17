c0fde6dc0c61f2348e56df371ae9dc6311e70cc3
Subin Min <tnqls5417@naver.com>
2020-11-17 23:40:40 +0900

    20201117

---
+++ baekjoon/[1117] 무작위_Gold1-2/[10999] 구간 합 구하기 2.cpp
@@ -0,0 +1,71 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, M, K;
+long long int arr[1000000];
+long long int tree[4000004], lazy[4000004];
+long long int init(int node, int start, int end) {
+    if (start == end)
+        return tree[node] = arr[start];
+    int mid = (start + end) / 2;
+    return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
+}
+void update_lazy(int node, int start, int end) {
+    if (lazy[node] != 0) {
+        tree[node] += lazy[node] * (end - start + 1);
+        if (start != end) {
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+}
+void update_range(int node, int start, int end, int left, int right,long long int diff) {
+    update_lazy(node, start, end);
+    if (left > end || right < start)
+        return;
+    if (left <= start && end <= right) {
+        tree[node] += diff * (end - start + 1);
+        if (start != end) {
+            lazy[node * 2] += diff;
+            lazy[node * 2+1] += diff;
+        }
+        return;
+    }
+    int mid = (start + end) / 2;
+    update_range(node * 2, start, mid, left, right, diff);
+    update_range(node * 2+1, mid+1, end, left, right, diff);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+long long sum(int node, int start, int end, int left, int right) {
+    update_lazy(node, start, end);
+    if (left > end || right < start)
+        return 0;
+    if (left <= start && end <= right)
+        return tree[node];
+    int mid = (start + end) / 2;
+    return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid + 1, end, left, right);
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N >> M >> K;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    init(1, 0, N - 1);
+    int a, b, c;
+    long long d;
+    for (int i = 0; i < M + K; i++) {
+        cin >> a;
+        if (a == 1) {
+            cin >> b >> c >> d;
+            update_range(1, 0, N - 1, b - 1, c - 1, d);
+        }
+        else {
+            cin >> b >> c;
+            cout << sum(1, 0, N - 1, b-1, c-1) << '\n';
+        }
+    }
+    return 0;
+}
---
+++ baekjoon/[1117] 무작위_Gold1-2/[1725] 히스토그램.cpp
@@ -0,0 +1,51 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N,tree[400001], arr[100000];
+int init(int node, int start, int end) {
+    if (start == end)
+        return tree[node] = start;
+    int mid = (start + end) / 2;
+    int temp1 = init(node * 2, start, mid);
+    int temp2 = init(node * 2 + 1, mid + 1, end);
+    if (arr[temp1] < arr[temp2])
+        return tree[node] = temp1;
+    else
+        return tree[node] = temp2;
+}
+int h(int node, int start, int end, int left, int right) {//가장 공통(낮은) 높이 인덱스
+    if (left>end||start>right)
+        return -1;
+    if (left <= start && end <= right)
+        return tree[node];
+    int mid = (start + end) / 2;
+    int temp1 = h(node * 2, start, mid, left, right);
+    int temp2 = h(node * 2 + 1, mid + 1, end, left, right);
+    if (temp1 == -1)
+        return temp2;
+    if (temp2 == -1)
+        return temp1;
+    if (arr[temp1] < arr[temp2])
+        return temp1;
+    else
+        return temp2;
+}
+int solve(int left, int right) {//공통 높이 기준으로 양옆으로 나눠서 넓이 최대 확인
+    int idx = h(1, 0, N - 1, left, right);
+    int area = arr[idx] * (right - left + 1);
+    if (idx != left)
+        area = max(area, solve(left, idx - 1));
+    if (idx != right)
+        area = max(area, solve(idx + 1, right));
+    return area;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    init(1, 0, N - 1);
+    cout << solve(0, N - 1);
+    return 0;
+}
