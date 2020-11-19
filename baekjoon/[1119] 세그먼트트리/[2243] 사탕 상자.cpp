d20122ab0840e0b7a5d06c6f43a8e26e4cdad96b
Subin Min <tnqls5417@naver.com>
2020-11-19 20:51:47 +0900

    20201119

---
+++ baekjoon/[1119] 세그먼트트리/[2243] 사탕 상자.cpp
@@ -0,0 +1,48 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int tree[1000001] = { 0 };
+void update(int i, int diff) {
+    while (i < 1000001) {
+        tree[i] += diff;
+        i += (i & -i);
+    }
+}
+int sum(int i) {
+    int ans = 0;
+    while (i > 0) {
+        ans += tree[i];
+        i -= (i & -i);
+    }
+    return ans;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    int a, b, c;
+    for (int i = 0; i < N; i++) {
+        cin >> a;
+        if (a == 1) {
+            cin >> b;
+            int s = 1, e = 1000001, m;
+            while (e - s > 0) {
+                m = (e + s) / 2;
+                if (sum(m) < b)
+                    s = m + 1;
+                else
+                    e = m;
+            }
+            cout << e << '\n';
+            update(e, -1);
+        }
+        else {
+            cin >> b >> c;
+            update(b, c);
+        }
+    }
+    return 0;
+}
---
+++ baekjoon/[1119] 세그먼트트리/[3653] 영화 수집.cpp
@@ -0,0 +1,45 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int T,N,M;
+int arr[100001];
+int tree[200001];
+void update(int i, int diff) {
+    while (i < 200001) {
+        tree[i] += diff;
+        i += (i & -i);
+    }
+}
+int sum(int i) {
+    int ans = 0;
+    while (i > 0) {
+        ans += tree[i];
+        i -= (i & -i);
+    }
+    return ans;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        cin >> N >> M;
+        for (int i = 1; i <= N + M; i++) {
+            tree[i] = 0;
+        }
+        for (int i = 1; i <= N; i++) {
+            arr[i] = M + i;
+            update(M + i, 1);
+        }
+        int temp;
+        for (int i = 0; i < M; i++) {
+            cin >> temp;
+            update(M-i, 1);
+            update(arr[temp], -1);
+            cout << sum(arr[temp]) - 1 << " ";
+            arr[temp] = M-i;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
