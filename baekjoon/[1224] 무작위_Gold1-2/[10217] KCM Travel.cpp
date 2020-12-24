52b92c444b1256095294334d7580650fef4434b2
Subin Min <tnqls5417@naver.com>
2020-12-24 23:01:19 +0900

    20201224

---
+++ baekjoon/[1224] 무작위_Gold1-2/[10217] KCM Travel.cpp
@@ -0,0 +1,58 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int dp[101][10001];
+int T, N, M, K, INF = 987654321;
+vector<pair<pair<int, int>, int>> edge[101];
+//비용, 목적지
+void dijkstra() {
+    priority_queue<pair<pair<int, int>, int>> pq;
+    pq.push(make_pair(make_pair(0, 0), 1));
+    while (!pq.empty()) {
+        int xd = -pq.top().first.first;//시간
+        int xc = -pq.top().first.second;
+        int x = pq.top().second;
+        if (x == N)
+            return;
+        pq.pop();
+        if (dp[x][xc]!=-1&&dp[x][xc] < xd) continue;
+        for (int i = 0; i < edge[x].size(); i++) {
+            int n = edge[x][i].first.first;
+            int nc = edge[x][i].first.second;
+            int nd = edge[x][i].second;//시간
+            if (nc + xc <= M && (dp[n][xc + nc] == -1||dp[n][xc+nc]>nd+xd)) {//dp[n][xc+nc]>nd+xd
+                dp[n][xc + nc] = nd + xd;
+                pq.push(make_pair(make_pair(-dp[n][xc + nc], -(xc + nc)),n));
+            }
+        }
+    }
+
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        cin >> N >> M >> K;
+        for (int i = 1; i <= N; i++)
+            edge[i].clear();
+        int u, v, c, d;
+        for (int i = 0; i < K; i++) {
+            cin >> u >> v >> c >> d;
+            edge[u].push_back(make_pair(make_pair(v, c), d));
+        }
+        memset(dp, -1, sizeof(dp));
+        dp[1][0] = 0;
+        dijkstra();
+        int result = INF;
+        for (int i = 0; i <= M; i++) {
+            if (dp[N][i] != -1)
+                result = min(dp[N][i], result);
+        }
+        if (result == INF)
+            cout << "Poor KCM\n";
+        else
+            cout << result << "\n";
+    }
+    return 0;
+}
---
+++ baekjoon/[1224] 무작위_Gold1-2/[4991] 로봇 청소기.cpp
@@ -0,0 +1,79 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int W, H, RESULT = 500000, cnt = 0;
+int arr[20][20], d[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
+int dp[20][20][1<<10];
+int ch[20][20];
+queue<pair<pair<int, int>,int>> q;
+void solve() {
+    while (!q.empty()) {
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int z = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int nx = x + d[i][0];
+            int ny = y + d[i][1];
+            int nz = z;
+            if (nx<0 || ny<0 || nx>W - 1 || ny>H - 1) continue;
+            if (arr[nx][ny] == -1) continue;
+            if (arr[nx][ny] == 1) nz |= (1 << ch[nx][ny]);
+            if (dp[nx][ny][nz] != -1) {
+                if (dp[x][y][z] + 1 < dp[nx][ny][nz]) {
+                    dp[nx][ny][nz] = dp[x][y][z] + 1;
+                    q.push(make_pair(make_pair(nx, ny), nz));
+                    if (nz == ((1 << cnt) - 1))
+                        RESULT = min(RESULT, dp[nx][ny][nz]);
+                }
+            }
+            else {
+                dp[nx][ny][nz] = dp[x][y][z] + 1;
+                q.push(make_pair(make_pair(nx, ny), nz));
+                if (nz == ((1 << cnt )- 1))
+                RESULT = min(RESULT, dp[nx][ny][nz]);
+            }
+
+
+        }
+    }
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    while (1) {
+        cin >> H >> W;
+        if (!W && !H)
+            break;
+        memset(dp, -1, sizeof(dp));
+        memset(ch, -1, sizeof(ch));
+        RESULT = 500000;
+        char temp;
+        cnt = 0;
+        for (int i = 0; i < W; i++) {
+            for (int j = 0; j < H; j++) {
+                cin >> temp;
+                if (temp == '*') {
+                    arr[i][j] = 1;
+                    ch[i][j] = cnt;
+                    cnt++;
+                }
+                else if (temp == 'x')
+                    arr[i][j] = -1;
+                else if (temp == 'o') {
+                    arr[i][j] = 0;
+                    q.push(make_pair(make_pair(i, j),0));
+                    dp[i][j][0] = 0;
+                }
+                else
+                    arr[i][j] = 0;
+            }
+        }
+        solve();
+        if (RESULT == 500000)
+            cout << -1 << "\n";
+        else
+            cout << RESULT << "\n";
+    }
+    return 0;
+}
