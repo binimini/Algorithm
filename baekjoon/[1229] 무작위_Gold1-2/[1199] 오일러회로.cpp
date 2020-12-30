29c6634305cdcbfe43c7306ccf61a4e726e2492f
Subin Min <tnqls5417@naver.com>
2020-12-30 22:55:49 +0900

    20201229

---
+++ baekjoon/[1229] 무작위_Gold1-2/[1199] 오일러회로.cpp
@@ -0,0 +1,53 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, C = 1;
+bool ex = 0;
+int adj[1001][1001] = { 0 };
+int degree[1001] = { 0 };
+/*
+7
+0 1 1 1 0 0 1
+1 0 1 0 0 0 0
+1 1 0 1 0 0 1
+1 0 1 0 1 1 0
+0 0 0 1 0 0 1
+0 0 0 1 0 0 1
+1 0 1 0 1 1 0
+*/
+void dfs(int node) {
+    for (int i = 1; i <= N; i++) {
+        if (adj[node][i]>0) {
+            adj[node][i]--;
+            adj[i][node]--;
+    //여기서 출력시 나중가야할 간선 먼저감(먼저 출력)
+            //1 2 3 1 4 3 7 >>1<< 5 4 6 7 1
+            dfs(i);
+        }
+    }
+    cout << node << " ";
+    //간선 다 없어진 경우 DFS호출시 출력 (따라서 역으로 출력됨)
+    // 1 7 6 4 5 7 3 4 1 3 2 1
+
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    int cnt = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> adj[i][j];
+            cnt += adj[i][j];
+        }
+        if (cnt % 2 != 0)
+            ex = 1;
+    }
+    if (!ex) {
+        dfs(1);
+        cout << "1\n";
+    }
+    else
+        cout << -1;
+    return 0;
+}
---
+++ baekjoon/[1229] 무작위_Gold1-2/[2213] 트리의 독립집합.cpp
@@ -0,0 +1,96 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N;
+int w[10001];
+vector<int> e[10001];
+int dp[10001][2];
+bool visited[10001] = { 0 };
+vector<pair<int, int>> v[10000][2];
+int solution(int node, int in) {
+    if (dp[node][in] != -1)
+        return dp[node][in];
+    visited[node] = 1;
+    bool visit = 1;
+    for (int i = 0; i < e[node].size(); i++) {
+        if (!visited[e[node][i]]) {
+            visit = 0;
+            break;
+        }
+    }
+    if (visit) {
+        visited[node] = 0;
+        if (in) return w[node];
+        else return 0;
+    }
+
+    int& r = dp[node][in];
+    r = 0;
+    for (int i = 0; i < e[node].size(); i++) {
+        if (visited[e[node][i]]) continue;
+        if (in) {
+            r += solution(e[node][i], 0);
+            v[node][in].push_back({ e[node][i], 0 });
+        }
+        else {
+            int t1 = solution(e[node][i], 0);
+            int t2 = solution(e[node][i], 1);
+            if (t1 < t2) {
+                v[node][in].push_back({ e[node][i], 1 });
+                r += t2;
+            }
+            else {
+                v[node][in].push_back({ e[node][i], 0 });
+                r += t1;
+            }
+        }
+    }
+    if (in)
+        r += w[node];
+    visited[node] = 0;
+    return r;
+}
+void tracking(int r) {
+    queue<pair<int,int>> q;
+    int n, b;
+    if (dp[1][1] == r)
+        q.push({ 1,1 });
+    else
+        q.push({ 1,0 });
+    vector<int> result;
+    while (!q.empty()) {
+        n = q.front().first;
+        b = q.front().second;
+        q.pop();
+        if (b)
+            result.push_back(n);
+        for (int i = 0; i < v[n][b].size(); i++) {
+            q.push(v[n][b][i]);
+        }
+    }
+    sort(result.begin(), result.end());
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << "\n";
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> w[i];
+    }
+    int a, b;
+    for (int i = 0; i < N - 1; i++) {
+        cin >> a >> b;
+        e[a].push_back(b);
+        e[b].push_back(a);
+    }
+
+    memset(dp, -1, sizeof(dp));
+    int result = max(solution(1, 0), solution(1, 1));
+    cout << result<<"\n";
+    tracking(result);
+    return 0;
+}
