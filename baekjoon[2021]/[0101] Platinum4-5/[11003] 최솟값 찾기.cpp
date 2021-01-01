ddad58340e0e443cbb80c7f80a60b191394d0772
Subin Min <tnqls5417@naver.com>
2021-01-01 22:32:09 +0900

    20210101

---
+++ baekjoon[2021]/[0101] Platinum4-5/[11003] 최솟값 찾기.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+deque<pair<int,int>> dq;
+int N, L;
+int main() {
+    FASTIO;
+    //슬라이딩 윈도우 (최솟값) O(N)
+    //1. deq 최대 L개의 숫자
+    //2. 오름차순 정렬(최솟값 바로 접근 가능), i번째에서 최대 큰 값 == arr[i]
+    cin >> N >> L;
+    int temp, idx = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        if (!dq.empty() && dq.front().second <= i - L)//한 턴에서 나가는 건 최대 한개
+            dq.pop_front();
+        while (!dq.empty() && dq.back().first > temp)// arr[i]보다 크면서 이미 존재하는 건 여러개 가능
+            dq.pop_back();
+        dq.push_back({ temp,i });
+        cout << dq.front().first << " ";
+    }
+    cout << "\n";
+    return 0;
+}
---
+++ baekjoon[2021]/[0101] Platinum4-5/[2188] 축사 배정.cpp
@@ -0,0 +1,42 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0);
+int N, M, res = 0, B[201];
+vector<int> edge[201];
+bool visited[201] = { 0 };
+bool dfs(int node) {//이분 매칭 
+    if (visited[node]) return 0;//이미 방문한 정점일경우 false
+    visited[node] = 1;
+    for (int i = 0; i < edge[node].size(); i++) {
+        int b = edge[node][i];
+        if (B[b] == -1 || dfs(B[b])) {//B 그룹에서 매칭 안되있거나 해당 b 매칭 a (B[b])가 다른 점 매칭 가능 시
+            B[b] = node;
+            return 1;
+        }
+    }
+    return 0;
+}
+void match() {
+    for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++)//정점 별 매칭 가능 여부 확인시마다 visited 초기화해야한다. 
+            //visited 정점 매칭 내에서의 방문여부
+            visited[i] = 0;
+        if (dfs(i)) res++;//이미 매칭한 것 기반으로 새로운 매칭 가능하면 매칭 수 늘린다.
+    }
+}
+int main() {
+    FASTIO;
+    cin >> N >> M;
+    memset(B, -1, sizeof(B));
+    int size, temp;
+    for (int i = 1; i <= N; i++) {
+        cin >> size;
+        for (int j = 0; j < size; j++) {
+            cin >> temp;
+            edge[i].push_back(temp);
+        }
+    }
+    match();
+    cout << res;
+    return 0;
+}
