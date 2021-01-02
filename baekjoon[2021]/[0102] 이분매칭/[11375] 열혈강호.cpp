0364fb10f71c2b4fab86db3c964daede0c0ac1f6
Subin Min <tnqls5417@naver.com>
2021-01-02 21:26:02 +0900

    20210102

---
+++ baekjoon[2021]/[0102] 이분매칭/[11375] 열혈강호.cpp
@@ -0,0 +1,40 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, M;
+bool visited[1001] = { 0 };
+vector<int> edge[1001];
+int staff_from_task[1001] = { 0 };
+bool dfs(int node) {
+    if (visited[node]) return 0;
+    visited[node] = 1;
+    for (int i = 0; i < edge[node].size(); i++) {
+        int next = edge[node][i];
+        if (staff_from_task[next] == 0 || dfs(staff_from_task[next])) {
+            staff_from_task[next] = node;
+            return 1;
+        }
+    }
+    return 0;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    int size, temp;
+    for (int i = 1; i <= N; i++) {
+        cin >> size;
+        for (int j = 0; j < size; j++) {
+            cin >> temp;
+            edge[i].push_back(temp);
+        }
+    }
+    int res = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++)
+            visited[j] = 0;
+        if (dfs(i))
+            res++;
+    }
+    cout << res;
+    return 0;
+}
---
+++ baekjoon[2021]/[0102] 이분매칭/[1298] 노트북의 주인을 찾아서.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, M;
+bool visited[101] = { 0 };
+int B[101] = { 0 };//연결할 i번째 노드가 연결 되어있는 A그룹 정보
+vector<int> edge[101];
+bool dfs(int node) {
+    if (visited[node]) return false;
+    visited[node] = 1;
+    for (int i = 0; i < edge[node].size(); i++) {
+        if (B[edge[node][i]] == 0 || dfs(B[edge[node][i]])) {
+            B[edge[node][i]] = node;
+            return true;
+        }
+    }
+    return false;
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    int a, b;
+    for (int i = 0; i < M; i++) {
+        cin >> a >> b;
+        edge[a].push_back(b);
+    }
+    int res = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++)
+            visited[j] = 0;
+        if (dfs(i))
+            res++;
+    }
+    cout << res;
+    return 0;
+}
