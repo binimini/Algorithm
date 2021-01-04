f99501d53060a3531443f699997b09ae70b11d57
Subin Min <tnqls5417@naver.com>
2021-01-04 23:46:15 +0900

    20210104

---
+++ baekjoon[2021]/[0104] Platinum2-3/[1126] 같은 탑.cpp
@@ -0,0 +1,42 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, dp[51][500001], h[51];
+int main() {
+    //dp[i][j] = i번째까지의 블럭(쓸수있는 범위가) h1>h2일 떄 h1-h2=j 에서 최대 h1 값
+    //dp[N][0] -> N번째까지의 블럭으로 h1-h2=0 즉 h1=h2일떄 최대 h1 값
+    //문제에서 원하는 바를 메모리 제한 내에 배열로 표현할수 있게 생각 해보고 // 점화식 상황에 따라 구해내는 방식????
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> h[i];
+    }
+    memset(dp, -1, sizeof(dp));
+    dp[0][0] = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= 500000; j++) {
+            //i번째 블록 사용X
+            if (dp[i - 1][j] != -1)
+                dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+            //h2에 블록 사용해 차이 줄이는 경우
+            if (dp[i - 1][j + h[i]] != -1)
+                dp[i][j] = max(dp[i][j], dp[i - 1][j + h[i]]);
+            //h1에 블록 사용하는 경우
+            if (h[i] <= j) {//사용 블록보다 차이가 크면 -> 차이가 늘어나고 높이도 늘어난다
+                if (dp[i - 1][j - h[i]] != -1) {
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - h[i]] + h[i]);
+                }
+            }
+            else if (h[i] > j) {//사용 블록이 차이보다 크면 -> dp[i][j]에서의 h2가 h1이었던 차이가 h[i]-j인 최대 높이에 j를 더한다
+                //dp[i-1][h[i]-j](과거h1==현재h2) - (h[i]-j)(차이, 과거h2==현재h1의 과거 높이)  + h[i](현재 h1 높이)
+                if (dp[i - 1][h[i] - j] != -1) {
+                    dp[i][j] = max(dp[i][j], dp[i - 1][h[i] - j] + j);
+                }
+            }
+        }
+    }
+
+    if (dp[N][0] <= 0)//0(<-dp[0][0])이거나 최고높이 있거나
+        cout << -1;
+    else
+        cout << dp[N][0];
+    return 0;
+}
---
+++ baekjoon[2021]/[0104] Platinum2-3/[1395] 스위치.cpp
@@ -0,0 +1,66 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int N, M;
+int tree[400001] = { 0 };
+bool lazy[400001] = { 0 };
+void update_lazy(int node, int start, int end) {//lazy값 먼저 적용 child 있을 경우 lazy값 물려주기(합의 경우 원소 수만큼)
+
+    if (lazy[node] != 0) {
+        tree[node] = end - start + 1 - tree[node];
+        if (start != end) {
+            int mid = (start + end) / 2;
+            lazy[node * 2] = !lazy[node * 2];//전체 스위치 반전 여부!! 
+            //직접 개수 계산하려하지말고 반전 여부만 처리해놓은뒤 개수 필요할때 반전 여부에 따라 계산한다.
+            lazy[node * 2 + 1] = !lazy[node * 2 + 1];
+        }
+        lazy[node] = 0;
+    }
+}
+void update_range(int node, int start, int end, int left, int right) {//query 함수랑 비슷한 형태
+    
+    update_lazy(node, start, end);//이미 lazy 있다면 처리
+
+    if (right < start || end < left)
+        return;
+    int mid = (start + end) / 2;
+    if (left <= start && end <= right) {//노드 범위가 구간 내에 전부 포함될 때만 lazy 이용 가능
+        tree[node] = end - start + 1 - tree[node];
+        if (start != end) {
+            lazy[node * 2] = !lazy[node * 2];
+            lazy[node * 2 + 1] = !lazy[node * 2 + 1];
+        }
+        return;
+    }
+
+    update_range(node * 2, start, mid, left, right);//아니면 더 파고들것
+    update_range(node * 2 + 1, mid + 1, end, left, right);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];//tree[node] 업데이트 잊지말것!!
+
+}
+int query(int node, int start, int end, int left, int right) {
+
+    update_lazy(node, start, end);//실제 값 구하기 전에도 lazy 남아 있을 수 있으므로 처리 필요!!
+    
+    if (right < start || end < left)
+        return 0;
+    if (left <= start && end <= right)
+        return tree[node];
+    int mid = (start + end) / 2;
+    return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
+}
+int main() {
+    FASTIO;
+    cin >> N >> M;
+    int o, s, t;
+    for (int i = 0; i < M; i++) {
+        cin >> o >> s >> t;
+        if (o == 0) {
+            update_range(1, 0, N - 1, s - 1, t - 1);
+        }
+        else if (o == 1) {
+            cout << query(1, 0, N - 1, s - 1, t - 1) << "\n";
+        }
+    }
+    return 0;
+}
