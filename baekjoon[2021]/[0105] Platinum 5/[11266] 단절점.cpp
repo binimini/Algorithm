d8fe4d4f91ef24937a351a5a6762b63b2e4498b4
Subin Min <tnqls5417@naver.com>
2021-01-05 22:09:39 +0900

    20210105

---
+++ baekjoon[2021]/[0105] Platinum 5/[11266] 단절점.cpp
@@ -0,0 +1,64 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int V, E, cnt = 1;
+bool iscutv[10001] = { 0 };
+int visited[10001] = { 0 };
+vector<int> edge[10001];
+int dfs(int node, bool root) {//단절점과 단절선 //양방향 간선이므로 일방 dfs스패닝트리를 만들어 접근한다!!
+    int ret = visited[node];
+    int child = 0;
+    //더 갈 곳 없을 경우 dfs 여기서 끝나므로 단절점 아니게됨
+    for (int i = 0; i < edge[node].size(); i++) {
+        int next = edge[node][i];
+        if (!visited[next]) {
+            child++;
+            visited[next] = cnt++;
+            int low = dfs(next, 0);
+            if (!root && low >= visited[node]) {//node의 자식노드가 visited[node] 이전 노드에 방문할 수 없다면 절단점
+                //(dfs 값 사이클 같은 경우 자기자신까지 가능)
+                //(우회로 존재하는 다른 자식노드 있을 수 있지만 이미 connected component 늘어나게 되므로 cutv)
+                //if low<visited[node] 우회로 존재 -> cutv가 아님으로 판정 시 우회로 존재하지않는 자식노드 있음에도 notcutv로 판정 가능 주의!!
+                iscutv[node] = 1;
+            }
+            ret = min(ret, low);
+        }
+        else
+            ret = min(ret, visited[next]);//이미 방문한 점이어도 이전값 확인은 해야함
+    }
+    //root의 경우 !visited[next]인 개수 세서 판정해야한다 왜 edge[node].size()로 판정하면 안될까??
+    //-> ex) 루트가 사이클 내 점일 경우 edge[node].size는 2이지만 루트를 끊어내도 connected component는 하나다.
+    if (root)
+        iscutv[node] = (child >= 2);
+    return ret;
+}
+int main() {
+    FASTIO;
+    cin >> V >> E;
+    int a, b;
+    for (int i = 0; i < E; i++) {
+        cin >> a >> b;
+        edge[a].push_back(b);
+        edge[b].push_back(a);//양방향 그래프
+    }
+
+    for (int i = 1; i <= V; i++) {
+        if (!visited[i]) {
+            visited[i] = cnt++;
+            dfs(i, 1);
+        }
+    }
+    vector<int> result;
+    int res = 0;
+    for (int i = 1; i <= V; i++) {
+        if (iscutv[i]) {
+            res++;
+            result.push_back(i);
+        }
+    }
+    cout << res << "\n";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    return 0;
+}
---
+++ baekjoon[2021]/[0105] Platinum 5/[2618] 경찰차.cpp
@@ -0,0 +1,61 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int N, W, dp[1001][1001], INF=10000000;
+pair<int, int> Incident[1001];
+int solution(int i, int j) {
+    if (i == W || j == W)
+        return 0;
+    int& r = dp[i][j];
+    if (r != -1)
+        return r;
+    r = 0;
+    int next = max(i, j) + 1;
+    int dist1, dist2;
+    if (i == 0)
+        dist1 = abs(Incident[next].first - 1) + abs(Incident[next].second - 1);
+    else
+        dist1 = abs(Incident[next].first - Incident[i].first) + abs(Incident[next].second - Incident[i].second);
+
+    if (j == 0)
+        dist2 = abs(Incident[next].first - N) + abs(Incident[next].second - N);
+    else
+        dist2 = abs(Incident[next].first - Incident[j].first) + abs(Incident[next].second - Incident[j].second);
+    
+    return r = min(dist1 + solution(next, j), dist2 + solution(i, next));
+
+}
+void tracking(int i, int j) {
+    if (i == W || j == W)
+        return;
+    int next = max(i, j) +1;
+    int distI, distJ;
+    if (i==0)
+        distI = abs(Incident[next].first - 1) + abs(Incident[next].second - 1);
+    else 
+        distI = abs(Incident[next].first - Incident[i].first) + abs(Incident[next].second - Incident[i].second);
+    if (j==0)
+        distJ = abs(Incident[next].first - N) + abs(Incident[next].second - N);
+    else 
+        distJ = abs(Incident[next].first - Incident[j].first) + abs(Incident[next].second - Incident[j].second);
+    if (dp[next][j] + distI > dp[i][next] + distJ) {
+        cout << 2 << "\n";
+        tracking(i, next);
+    }
+    else {
+        cout << 1 << "\n";
+        tracking(next, j);
+    }
+}
+int main() {
+    FASTIO;
+    cin >> N >> W;
+    for (int i = 1; i <= W; i++) {
+        cin >> Incident[i].first >> Incident[i].second;
+    }
+    memset(dp, -1, sizeof(dp));
+    cout <<solution(0, 0)<<"\n";
+    tracking(0,0);
+
+    return 0;
+}
