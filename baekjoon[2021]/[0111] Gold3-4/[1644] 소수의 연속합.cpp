da76e7382411a08876affca5c666846554d31e89
Subin Min <tnqls5417@naver.com>
2021-01-11 22:04:01 +0900

    20210111

---
+++ baekjoon[2021]/[0111] Gold3-4/[1644] 소수의 연속합.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, res = 0;//dp [prime idx][prime sum] maximum 3*10^5*4*10^6 =12*10^11??
+vector<int> v;
+bool notprime[4000001] = { 0 };
+int main() {
+    cin >> N;
+    for (int i = 2; i * i <= N; i++) {
+        if (!notprime[i]) {
+            for (int j = i*2; j <= N; j += i) {
+                notprime[j] = 1;
+            }
+        }
+    }
+    for (int i = 2; i <= N; i++) {//4000000 prime size about 300000
+        if (!notprime[i])
+            v.push_back(i);
+    }
+    int s = 0, e = 0, sum = 0;
+    while (e<=v.size()) {//two pointer
+        if (sum < N) {
+            if (e == v.size())//exception (outbound RTE)
+                break;
+            sum += v[e];
+            e++;
+        }
+        else if (sum >= N) {
+            sum -= v[s];
+            s++;
+        }
+        if (sum == N)
+            res++;
+    }
+    cout << res;
+    return 0;
+}
---
+++ baekjoon[2021]/[0111] Gold3-4/[1987] 알파벳.cpp
@@ -0,0 +1,32 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int R, C, d[4][2] = { {1,0},{0,1},{-1,0},{0,-1} }, res = 0;
+char mapp[21][21];
+bool alpha[26] = { 0 };
+queue<pair<int, int>> q;
+void dfs(int x, int y,int cnt) {//bfs 방문체크 못하기때문에 중복 발생 많다 -> case는 적어도 memory over 가능 => dfs??
+    res = max(res, cnt);
+    for (int i = 0; i < 4; i++) {
+        int nx = x + d[i][0];
+        int ny = y + d[i][1];
+        if (nx<0 || nx>R - 1 || ny<0 || ny>C - 1) continue;
+        if (alpha[mapp[nx][ny] - 'A']) continue;
+        alpha[mapp[nx][ny] - 'A'] = 1;
+        dfs(nx, ny, cnt+1);
+        alpha[mapp[nx][ny] - 'A'] = 0;
+    }
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> R >> C;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> mapp[i][j];
+        }
+    }
+    alpha[mapp[0][0] - 'A'] = 1;
+    dfs(0, 0, 1);
+    cout << res;
+    return 0;
+}
