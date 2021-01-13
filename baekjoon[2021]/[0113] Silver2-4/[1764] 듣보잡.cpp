83b0789f3ceab8a8c21c1758702afd341b68f0bc
Subin Min <tnqls5417@naver.com>
2021-01-13 23:55:11 +0900

    20210113 map

---
+++ baekjoon[2021]/[0113] Silver2-4/[1764] 듣보잡.cpp
@@ -0,0 +1,27 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int N, M, res = 0;
+map<string, int> m;
+int main() {
+    FASTIO;
+    cin >> N >> M;
+    string temp;
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        m[temp] = 1;
+    }
+    for (int j = 0; j < M; j++) {
+        cin >> temp;
+        if (m.find(temp) != m.end()) {
+            res++;
+            m[temp]++;
+        }
+    }
+    cout << res << "\n";
+    for (auto iter = m.begin(); iter != m.end(); iter++) {
+        if (iter->second == 2)
+            cout << iter->first << "\n";
+    }
+    return 0;
+}
