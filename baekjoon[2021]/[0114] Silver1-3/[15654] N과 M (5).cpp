7f2f5f3506b4867470a6f5b880b983cd5fe209eb
Subin Min <tnqls5417@naver.com>
2021-01-15 00:50:55 +0900

    20210114 dfs backtracking

---
+++ baekjoon[2021]/[0114] Silver1-3/[15654] N과 M (5).cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int N, M, arr[8], choice[8];
+bool visited[8] = { 0 };
+void dfs(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < cnt; i++) {
+            cout << arr[choice[i]] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 0; i < N; i++) {
+        if (visited[i]) continue;
+        choice[cnt] = i;
+        visited[i] = 1;
+        dfs(cnt + 1);
+        visited[i] = 0;
+    }
+}
+int main() {
+    FASTIO;
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+    dfs(0);
+    return 0;
+}
---
+++ baekjoon[2021]/[0114] Silver1-3/[15657] N과 M (8).cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int N, M, arr[8], choice[8];
+bool visited[8] = { 0 };
+void dfs(int node, int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < cnt; i++) {
+            cout << arr[choice[i]] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = node; i < N; i++) {
+        choice[cnt] = i;
+        dfs(i, cnt + 1);
+    }
+}
+int main() {
+    FASTIO;
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+    dfs(0,0);
+    return 0;
+}
