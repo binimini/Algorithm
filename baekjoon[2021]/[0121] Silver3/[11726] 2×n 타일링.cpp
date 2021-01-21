f6344fee5926e3d87bc426de5687e5a363940885
Subin Min <tnqls5417@naver.com>
2021-01-21 21:53:22 +0900

    11726 : dp

---
+++ baekjoon[2021]/[0121] Silver3/[11726] 2×n 타일링.cpp
@@ -0,0 +1,19 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define mod 10007
+int N, dp[1001];
+int solve(int n) {
+    if (n == 0)
+        return 1;
+    if (n < 0)
+        return 0;
+    if (dp[n] != -1)
+        return dp[n];
+    return dp[n] = (solve(n - 1) + solve(n - 2))%mod;
+}
+int main() {
+    memset(dp, -1, sizeof(dp));
+    cin >> N;
+    cout <<solve(N)%mod;
+    return 0;
+}
