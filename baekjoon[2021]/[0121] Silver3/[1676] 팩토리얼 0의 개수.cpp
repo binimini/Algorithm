c57afd1245dd35d77298b0023d97a4224bd698ad
Subin Min <tnqls5417@naver.com>
2021-01-21 21:52:40 +0900

    1676 : math

---
+++ baekjoon[2021]/[0121] Silver3/[1676] 팩토리얼 0의 개수.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N;
+int main() {
+    cin >> N;
+    //number of 0 == number of (5*2) == min(countof5,countof2) for factorial N
+    int cnt_2 = 0, cnt_5 = 0;
+    int i = 1, temp; 
+    while (i <= N) {//can't solve with factorization?
+        temp = i;
+        while (temp % 2 == 0) {
+            temp /= 2;
+            cnt_2++;
+        }
+
+        while (temp % 5 == 0) {
+            temp /= 5;
+            cnt_5++;
+        }
+        i++;
+    }
+    cout << min(cnt_2, cnt_5);
+    return 0;
+}
