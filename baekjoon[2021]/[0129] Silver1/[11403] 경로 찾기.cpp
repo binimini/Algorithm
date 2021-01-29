19e60dff2dffa72defe2b38958d570ab4ddcc029
Subin Min <tnqls5417@naver.com>
2021-01-29 17:28:57 +0900

    11403 : Floyd Warshall

---
+++ baekjoon[2021]/[0129] Silver1/[11403] 경로 찾기.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int N;
+bool can[101][101] = { 0 };
+int main() {
+    FASTIO;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> can[i][j];
+        }
+    }
+    for (int k = 1; k <= N; k++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (can[i][k] && can[k][j])
+                    can[i][j] = 1;
+            }
+        }
+    }
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cout << can[i][j] << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
