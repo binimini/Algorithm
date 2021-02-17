c398cfabbb4008e8eda16f0acddec82de47671a7
Subin Min <tnqls5417@naver.com>
2021-02-17 20:26:38 +0900

    2018 : Two pointer

---
+++ baekjoon[2021]/[2018] 수들의 합 5.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, res = 0;
+int solve(int n) {
+    int s = 1, e = 1, sum = 0;
+    while (e<=N+1) {
+        if (sum >= N)
+            sum -= s++;
+        else if (sum < N) {
+            if (e == N + 1)
+                break;
+            else
+                sum += e++;
+        }
+        if (sum == N)
+            res++;
+    }
+    return res;
+}
+int main() {
+    cin >> N;
+    cout << solve(N);
+    return 0;
+}
