bda2652a666e92d2dd2a9dee9a57ef27429e28cd
Subin Min <tnqls5417@naver.com>
2021-02-17 20:28:58 +0900

    8892 : Brute force

---
+++ baekjoon[2021]/[8892] 팰린드롬.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int T, N;
+string arr[100], res;
+bool flag = 0;
+bool check(string str) {
+    for (int i = 0; i < str.size() / 2; i++) {
+        if (str[i] != str[str.size() - 1 - i])
+            return false;
+    }
+    return true;
+}
+int main() {
+    FASTIO;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        flag = 0;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                res = arr[i] + arr[j];
+                if (check(res)) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag)
+                break;
+        }
+        if (flag)
+            cout << res << "\n";
+        else
+            cout << 0 << "\n";
+    }
+    return 0;
+}
