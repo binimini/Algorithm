aaa3f62db8ce17b39b39594a424cd803e5a9f7ed
Subin Min <tnqls5417@naver.com>
2021-02-19 20:46:10 +0900

    13171 : Divide Conquer

---
+++ baekjoon[2021]/[13171] A.cpp
@@ -0,0 +1,32 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+long long int A, AA = 1, X, XX;
+int p[65];
+int MOD = 1000000007;
+int main() {
+    cin >> A >> X;
+    int i = 1;
+    XX = 1;
+    AA = A % MOD;
+    p[0] = 1;
+    while (XX <= X) {
+        p[i] = AA % MOD;
+        XX *= 2;
+        AA = ((AA % MOD) * (AA % MOD)) % MOD;
+        i++;
+    }
+    XX /= 2;
+    i--;
+    long long int xx = 0, ans = 1;//long long int!!!
+    while (xx != X) {
+        if (xx+XX <= X) {
+            xx += XX;
+            ans = ((ans % MOD) * (p[i] % MOD)) % MOD;
+        }
+        XX /= 2;
+        i--;
+    }
+    cout << ans;
+
+    return 0;
+}
