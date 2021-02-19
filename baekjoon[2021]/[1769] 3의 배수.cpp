bc86a69b16ac6677c7d0127564ea950b632fd405
Subin Min <tnqls5417@naver.com>
2021-02-19 20:46:47 +0900

    1769 : String

---
+++ baekjoon[2021]/[1769] 3의 배수.cpp
@@ -0,0 +1,22 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+long long int temp, cnt = 0, res = 0;
+string X, Y;
+int main() {
+    cin >> X;
+    while (X.size() >= 2) {
+        cnt++;
+        temp = 0;
+        for (int i = 0; i < X.size(); i++) {//자리수가 10^6까지
+            temp += X[i] - '0';
+        };
+        X = to_string(temp);
+    }
+    res = stoi(X);
+    cout << cnt << "\n";
+    if (res % 3 == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
