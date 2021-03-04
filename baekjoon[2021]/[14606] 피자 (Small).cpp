535dc166e3c15274fc7b3e1bf315aeb86accd7a6
Subin Min <tnqls5417@naver.com>
2021-03-04 22:50:12 +0900

    14606 : DP

---
+++ baekjoon[2021]/[14606] 피자 (Small).cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, dp[11];
+int main() {
+    cin >> N;
+    memset(dp, -1, sizeof(dp));
+    dp[0] = 0;
+    dp[1] = 0;
+    for (int i = 2; i <= N; i++) {
+        dp[i] = 0;
+        for (int j = 1; j <= N / 2; j++) {
+            dp[i] = max(dp[i], dp[j] + dp[i - j] + j * (i - j));
+        }
+    }
+    cout << dp[N];
+    return 0;
+}
