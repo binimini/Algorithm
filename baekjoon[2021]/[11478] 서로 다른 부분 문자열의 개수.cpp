6cb8f34de6c71fee02eaf23b27c054711c8564c0
Subin Min <tnqls5417@naver.com>
2021-03-04 22:48:04 +0900

    11478 : Map

---
+++ baekjoon[2021]/[11478] 서로 다른 부분 문자열의 개수.cpp
@@ -0,0 +1,14 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+string s;
+map<string, int> m;
+int main() {
+    cin >> s;
+    for (int i = 1; i <= s.size(); i++) {
+        for (int j = 0; j <= s.size() - i; j++) {
+            m[s.substr(j, i)] = 1;
+        }
+    }
+    cout << m.size();
+    return 0;
+}
