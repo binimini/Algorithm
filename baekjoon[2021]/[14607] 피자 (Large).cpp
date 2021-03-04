84e7370e85d31ac720928ff22bdf159c40c47f91
Subin Min <tnqls5417@naver.com>
2021-03-04 22:50:27 +0900

    14607 : Math

---
+++ baekjoon[2021]/[14607] 피자 (Large).cpp
@@ -0,0 +1,10 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+long long int N;
+int main() {
+    cin >> N;
+    //result(N+1) : result(N) + 0(1) + N*1 
+    //== result + N // sum of 1~N
+    cout << (N - 1) * N / 2;
+    return 0;
+}
