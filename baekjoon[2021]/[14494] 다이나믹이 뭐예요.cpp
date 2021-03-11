476ea03d6d584b295951590118a0cb8b02804a08
Subin Min <tnqls5417@naver.com>
2021-03-11 22:16:17 +0900

    14494 : DP

---
+++ baekjoon[2021]/[14494] 다이나믹이 뭐예요.cpp
@@ -0,0 +1,15 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, M;
+long long int dp[1001][1001] = { 0 }, MOD = 1000000007;
+int main() {
+    cin >> N >> M;
+    dp[0][0] = 1;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            dp[i][j] = (dp[i - 1][j]%MOD + dp[i][j - 1]%MOD + dp[i - 1][j - 1]%MOD)%MOD;
+        }
+    }
+    cout << dp[N][M]%MOD;
+    return 0;
+}
