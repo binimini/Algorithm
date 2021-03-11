750d639a1afbaf5a4cf5f5b8fe990546aa191bfb
Subin Min <tnqls5417@naver.com>
2021-03-11 22:11:54 +0900

    14469 : Sort

---
+++ baekjoon[2021]/[14469] 소가 길을 건너간 이유 3.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0);
+int N;
+priority_queue<pair<int, int>> pq;
+int main() {
+    FASTIO;
+    cin >> N;
+    int a, b;
+    for (int i = 0; i < N; i++) {
+        cin >> a >> b;
+        pq.push(make_pair(-a, b));
+    }
+    int res = 0;
+    while (!pq.empty()) {
+        int x = -pq.top().first;
+        int y = pq.top().second;
+        pq.pop();
+        if (res < x)
+            res = x;
+        res += y;
+    }
+    cout << res;
+    return 0;
+}
