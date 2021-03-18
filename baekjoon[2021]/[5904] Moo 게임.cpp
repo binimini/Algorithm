c2e94ff09442a029f71997ca9432ecf57d9eb9c3
Subin Min <tnqls5417@naver.com>
2021-03-18 22:24:23 +0900

    5904 : Divide Conquer & DP

---
+++ baekjoon[2021]/[5904] Moo 게임.cpp
@@ -0,0 +1,37 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N;
+int dp[1001];
+char tracking(int n, int lo) {
+    if (n == 0) {
+        if (lo == 1)
+            return 'm';
+        else
+            return 'o';
+    }
+    if (lo <= dp[n - 1])
+        return tracking(n - 1, lo);
+    else if (lo > dp[n - 1] && lo <= dp[n - 1] + n + 3) {
+        if (lo == dp[n - 1] + 1)
+            return 'm';
+        else
+            return 'o';
+    }
+    else 
+        return tracking(n - 1, lo - dp[n - 1] - n - 3);
+}
+int main() {
+    cin >> N;
+    int sum = 0;
+    int i = 0;
+    dp[0] = 3;
+    while (1) {
+        i++;
+        dp[i] = dp[i-1] * 2 + i + 3;
+        if (dp[i] >= N)
+            break;
+    }
+    cout <<tracking(i, N);
+    
+    return 0;
+}
