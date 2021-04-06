6eaa653ac4040799519aa437dd25022a353b6f2e
Subin Min <tnqls5417@naver.com>
2021-04-06 20:05:42 +0900

    17212 : DP

---
+++ baekjoon[2021]/[17212] 달나라 토끼를 위한 구매대금 지불 도우미.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, dp[100001], INF = 100001;
+int main() {
+    cin >> N;
+    for (int i = 0; i <= N; i++)
+        dp[i] = INF;
+    dp[0] = 0;
+    for (int i = 0; i <= N; i++) {
+        dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+        dp[i + 2] = min(dp[i + 2], dp[i] + 1);
+        dp[i + 5] = min(dp[i + 5], dp[i] + 1);
+        dp[i + 7] = min(dp[i + 7], dp[i] + 1);
+    }
+    cout << dp[N];
+    return 0;
+}
