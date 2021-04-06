27ac3a735414e9a6a0addfa5e20d494d82d8ec0a
Subin Min <tnqls5417@naver.com>
2021-04-06 20:05:21 +0900

    14670 : Map

---
+++ baekjoon[2021]/[14670] 병약한 영정.cpp
@@ -0,0 +1,39 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
+int N, R, L;
+map<int, int> m;
+vector<int> v;
+int main() {
+    FASTIO;
+    cin >> N;
+    int symtom, medi_name;
+    for (int i = 0; i < N; i++) {
+        cin >> symtom >> medi_name;
+        m[symtom] = medi_name;
+    }
+    cin >> R;
+    int temp;
+    bool dead_flag = 0;
+    for (int i = 0; i < R; i++) {
+        v.clear();
+        dead_flag = 0;
+        cin >> L;
+        for (int j = 0; j < L; j++) {
+            cin >> temp;
+            if (m.find(temp) == m.end())
+                dead_flag = 1;
+            else
+                v.push_back(m[temp]);
+        }
+        if (dead_flag)
+            cout << "YOU DIED\n";
+        else {
+            for (int j = 0; j < v.size(); j++) {
+                cout << v[j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
