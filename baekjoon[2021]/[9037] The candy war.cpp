b4f11b56cef6385dfa3c063bfc6b526b0f0420d9
Subin Min <tnqls5417@naver.com>
2021-04-06 20:04:47 +0900

    9037 : Implementation

---
+++ baekjoon[2021]/[9037] The candy war.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int T, N;
+vector<int> v;
+int main() {
+    FASTIO;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        v.clear();
+        cin >> N;
+        int temp;
+        for (int i = 0; i < N; i++) {
+            cin >> temp;
+            v.push_back(temp);
+        }
+        bool flag = 0;
+        int prev, cnt = 0;
+        while (!flag) {
+            prev = -1;
+            flag = 1;
+            for (int i = 0; i < v.size(); i++) {
+                if (v[i] % 2 != 0)
+                    v[i]++;
+                if (prev == -1)
+                    prev = v[i];
+                else if (prev != v[i])
+                    flag = 0; 
+            }
+            if (flag)
+                break;
+            cnt++;
+            int half = v[v.size() - 1] / 2, temp;
+            for (int i = 0; i < v.size(); i++) {
+                v[i] /= 2;
+                temp = v[i];
+                v[i] += half;
+                half = temp;
+            }
+        }
+        cout << cnt << "\n";
+    }
+    return 0;
+}
