e5e94c9ec2be7a3971aa332ceadfddc509afb568
Subin Min <tnqls5417@naver.com>
2021-04-14 22:56:02 +0900

    17299 : Stack

---
+++ baekjoon[2021]/[17299] 오등큰수.cpp
@@ -0,0 +1,27 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+vector<int> s, ans;
+int N, F[1000001] = { 0 }, arr[1000001];
+int main() {
+    FASTIO;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+        F[arr[i]]++;
+    }
+    for (int i = N - 1; i >= 0; i--) {
+        while (!s.empty() && F[s.back()] <= F[arr[i]]) 
+            s.pop_back();
+        if (s.empty()) 
+            ans.push_back(-1);
+        else
+            ans.push_back(s.back());
+        s.push_back(arr[i]);
+    }
+    while (!ans.empty()) {
+        cout << ans.back() << " ";
+        ans.pop_back();
+    }
+    return 0;
+}
