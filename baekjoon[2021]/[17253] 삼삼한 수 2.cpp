f8cccbcbce0c2c4be837e31d1736b2cf3e5e05cf
Subin Min <tnqls5417@naver.com>
2021-05-05 01:03:38 +0900

    17253 : Math

---
+++ baekjoon[2021]/[17253] 삼삼한 수 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+long long int N, arr[40];
+bool flag = 0;
+int main() {
+    cin >> N;
+    int cnt = 39;
+    long long int test = 1;
+    for (int i = 0; i < 40; i++) {
+        arr[i] = test;
+        test *= 3;
+    }
+    while (cnt >= 0&&N!=0) {
+        if (arr[cnt]<= N) {
+            N -= arr[cnt];//pow로 연산시 제곱승 너무 커질 경우 오차 발생
+            flag = 1;
+        }
+        cnt--;
+    }
+    if (N == 0&&flag)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
