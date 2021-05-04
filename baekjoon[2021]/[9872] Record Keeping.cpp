d1f4239c4e8eb36445de0e1a54689ab27824e1be
Subin Min <tnqls5417@naver.com>
2021-05-04 23:56:11 +0900

    9872 : Greedy

---
+++ baekjoon[2021]/[9872] Record Keeping.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int N;
+string temp;
+vector<string> v;
+map<string, int> m;
+int main() {
+    FASTIO;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        v.clear();
+        for (int j = 0; j < 3; j++) {
+            cin >> temp;
+            v.push_back(temp);
+        }
+        sort(v.begin(), v.end());
+        m[v[0] + v[1] + v[2]]++;
+    }
+    int ans = 0;
+    for (auto iter = m.begin(); iter != m.end(); iter++) {
+        ans = max(ans, iter->second);
+    }
+    cout << ans;
+    return 0;
+}
