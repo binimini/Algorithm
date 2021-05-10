0bb707093e3098994061ba76ca36f9e58f56d32e
Subin Min <tnqls5417@naver.com>
2021-05-10 23:00:03 +0900

    2777 : Greedy

---
+++ baekjoon[2021]/[2777] 숫자 놀이.cpp
@@ -0,0 +1,38 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int T,N;
+vector<int> v;
+int main() {
+    FASTIO;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        cin >> N;
+        v.clear();
+        int cnt = 0;
+        bool flag = 0;
+        if (N == 1)
+            v.push_back(1);
+        while (N != 1) {
+            flag = 0;
+            for (int i = 9; i >= 2; i--) {
+                if (N % i == 0) {
+                    v.push_back(i);
+                    cnt++;
+                    flag = 1;
+                    N /= i;
+                    break;
+                }
+            }
+            if (!flag) {
+                v.clear();
+                break;
+            }
+        }
+        if (v.empty())
+            cout << -1<<"\n";
+        else
+            cout << v.size()<<"\n";
+    }
+    return 0;
+}
