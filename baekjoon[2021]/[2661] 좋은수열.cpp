9ebaa49c331e6dd8c061639feef06efe3f4a780f
Subin Min <tnqls5417@naver.com>
2021-05-13 00:04:30 +0900

    2661 : Backtracking

---
+++ baekjoon[2021]/[2661] 좋은수열.cpp
@@ -0,0 +1,38 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N;
+bool flag = 0;
+string answer;
+bool check(string str) {
+    for (int i = 2; i <= str.size(); i += 2) {
+        for (int j = 0; j <= str.size() - i; j++) {
+            string temp1 = str.substr(j, i/2);
+            string temp2 = str.substr(j + i / 2, i / 2);
+            if (temp1 == temp2)
+                return false;
+        }
+    }
+    return true;
+}
+void solve(string ans, int n) {
+    if (flag)
+        return;
+    if (n == N) {
+        answer = ans;
+        flag = 1;
+        return;
+    }
+    for (char i = '1'; i <= '3'; i++) {
+        if (flag)
+            return;
+        if (check(ans + i))
+            solve(ans + i, n + 1);
+    }
+}
+
+int main() {
+    cin >> N;
+    solve("", 0);
+    cout << answer;
+    return 0;
+}
