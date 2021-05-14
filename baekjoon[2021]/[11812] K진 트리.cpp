93c89ef1c33771c21a6d5b1e9304ceeb32cb305b
Subin Min <tnqls5417@naver.com>
2021-05-14 22:24:16 +0900

    11812 : LCA

---
+++ baekjoon[2021]/[11812] K진 트리.cpp
@@ -0,0 +1,33 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+long long int N;
+long long int K, Q;
+int main() {
+    cin >> N >> K >> Q;
+    FASTIO;
+    long long int x, y;
+    for (int i = 0; i < Q; i++) {
+        cin >> x >> y;
+        if (K == 1) {
+            cout <<abs(x - y)<<"\n";
+        }
+        else {
+            x += K - 2;
+            y += K - 2;
+            int cnt = 0;
+            while (x != y) {
+                if (x > y) {
+                    x = (x / K) + K - 2;
+                    cnt++;
+                }
+                else if (y>x) {
+                    y = (y / K) + K - 2;
+                    cnt++;
+                }
+            }
+            cout << cnt << "\n";
+        }
+    }
+    return 0;
+}
