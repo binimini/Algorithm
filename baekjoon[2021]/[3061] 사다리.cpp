e1ebe19fb5c51cf239a56d266f24b7b8a27fc8f7
Subin Min <tnqls5417@naver.com>
2021-05-17 21:37:07 +0900

    3061 : Sort

---
+++ baekjoon[2021]/[3061] 사다리.cpp
@@ -0,0 +1,30 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int T,N;
+int arr[1000];
+int main() {
+    FASTIO;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        int cnt = 0;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        int temp;
+        for (int i = N-1; i >=0; i--) {     //bubble sort
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    cnt++;
+                }
+            }
+        }
+        cout << cnt << "\n";
+
+    }
+    return 0;
+}
