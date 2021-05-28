08ef2e193f10265d585f0ffc6a4ed7b4a207755f
Subin Min <tnqls5417@naver.com>
2021-05-28 21:25:34 +0900

    2306 : DP

---
+++ baekjoon[2021]/[2306] 유전자.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int N, dp[501][501];
+string S;
+int solve(int i , int j) {
+    if (i + 1 > j)
+        return 0;
+    if (i + 1 == j) {
+        if (S[i] == 'a' && S[j] == 't' || S[i] == 'g' && S[j] == 'c')
+            return 2;
+        else
+            return 0;
+    }
+    if (dp[i][j] != -1)
+        return dp[i][j];
+    int& r = dp[i][j];
+    r = 0;
+    int temp1 = 0, temp2 = 0;
+    if (i >= N || j >= N || i < 0 || j < 0)
+        return 0;
+    if (S[i] == 'a' && S[j] == 't' || S[i] == 'g' && S[j] == 'c') {
+        r = max(r, 2 + solve(i + 1, j - 1));
+    }
+    for (int x = i; x < j; x++) {
+        r = max(r, solve(i, x) + solve(x + 1, j));
+    }
+    return r;
+}
+int main() {
+    cin >> S;
+    N = S.size();
+    memset(dp, -1, sizeof(dp));
+    cout << solve(0,N-1);
+    return 0;
+}
