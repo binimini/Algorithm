5d6a8790cd6340c2b8b2d23fa4af15bef2331a97
Subin Min <tnqls5417@naver.com>
2021-09-05 14:54:28 +0900

    1923 : GCD

---
+++ baekjoon[2021]/[1923] 최소공배수.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm> //max(), min()
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int N;
+int GCD(int m, int n) { //GCD(m,n)==GCD(n, m%n)
+    int mod;
+    while(n) {
+        mod = m % n;
+        m = n;
+        n = mod;
+    }
+    
+    return m;
+}
+int main() {
+    cin >> N; 
+    int a, b, gcd, lcm;
+    for (int n = 0; n < N; n++) {
+        cin >> a >> b;
+        
+        gcd = GCD(max(a, b), min(a, b));
+        lcm = a * b / gcd;
+        
+        cout << lcm << "\n";
+    }
+    return 0;
+}
