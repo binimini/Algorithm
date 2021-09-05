a5ffc7aa6c5666ff87b79a70cdcced7f7248bcd4
Subin Min <tnqls5417@naver.com>
2021-09-05 14:54:42 +0900

    2960 : Prime

---
+++ baekjoon[2021]/[2960] 에라토스테네스의 체.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int N, K, cnt = 0;
+bool notPrime[1001] = { 0 }; //notPrime[i] == false -> i 소수
+int checkPrime() {
+    for (int i = 2; i <= N; i++) {
+        if (notPrime[i]) continue;
+        if (++cnt==K) return i;
+       
+        for (int j = i*2; j <= N; j += i) {
+            if (notPrime[j]) continue;
+            
+            if (++cnt == K) return j;
+            notPrime[j] = true;
+        }
+    }
+}
+int main() {
+    cin >> N >> K;
+
+    cout << checkPrime();
+    
+    return 0;
+}
